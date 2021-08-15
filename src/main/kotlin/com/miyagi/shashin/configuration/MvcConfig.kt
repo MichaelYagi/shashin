71178ee739e3e1ace330980c38edf6eac7787c44
Michael Yagi <michaeltyagi@gmail.com>
2021-08-15 13:50:55 -0700

    Handle multiple configured directories

--- src/main/kotlin/com/miyagi/shashin/configuration/MvcConfig.kt
+++ src/main/kotlin/com/miyagi/shashin/configuration/MvcConfig.kt
@@ -7,6 +7,8 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 import org.springframework.web.servlet.resource.PathResourceResolver
+import java.io.File
+import java.nio.file.Paths
 
 @Configuration
 class MvcConfig : WebMvcConfigurer {
@@ -15,16 +17,32 @@ class MvcConfig : WebMvcConfigurer {
     private val relativeSidecarDir: String? = null
     @Value("\${app.api.version}")
     private val apiVersion: String? = null
-    @Value("\${app.mediaDir}")
-    private val mediaDir: String? = null
+    @Value("\${app.mediaDirs}")
+    private val mediaDirs: Array<String>? = null
 
     override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
         val rootPath = FileSystemResource("").file.absolutePath
-        val thumbnailDir = "file:///$rootPath$relativeSidecarDir/thumbnails/"
+        var thumbnailDir = "file:///$rootPath$relativeSidecarDir"+"thumbnails/"
+        thumbnailDir = thumbnailDir.replace('\\', '/').lowercase()
+
+        if (mediaDirs != null) {
+            for (mediaDir in mediaDirs) {
+                val _mediaDir = mediaDir.lowercase()
+                val parentDir = Paths.get(_mediaDir)
+                val parentDirString = parentDir.fileName.toString().lowercase()
+
+                registry
+                    .addResourceHandler("/api/$apiVersion/original/video/$parentDirString/**")
+                    .addResourceLocations("file:///$_mediaDir/")
+                    .setCachePeriod(3600)
+                    .resourceChain(true)
+                    .addResolver(PathResourceResolver())
+            }
+        }
 
         registry
-            .addResourceHandler("/api/$apiVersion/thumbnails/**","/api/$apiVersion/original/video/**")
-            .addResourceLocations(thumbnailDir, "file:///$mediaDir")
+            .addResourceHandler("/api/$apiVersion/thumbnails/**")
+            .addResourceLocations(thumbnailDir)
             .setCachePeriod(3600)
             .resourceChain(true)
             .addResolver(PathResourceResolver())
