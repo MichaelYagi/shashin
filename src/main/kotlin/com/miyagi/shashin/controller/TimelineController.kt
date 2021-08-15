97353dc252d781d65d3e673c56b7807155b28904
Michael Yagi <michaeltyagi@gmail.com>
2021-08-14 21:16:29 -0700

    Save batch edits to database and metadata files

--- src/main/kotlin/com/miyagi/shashin/controller/TimelineController.kt
+++ src/main/kotlin/com/miyagi/shashin/controller/TimelineController.kt
@@ -1,10 +1,13 @@
 package com.miyagi.shashin.controller
 
+import com.fasterxml.jackson.core.type.TypeReference
+import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
 import com.fasterxml.jackson.dataformat.yaml.YAMLGenerator
 import com.miyagi.shashin.model.Metadata
 import com.miyagi.shashin.repository.MetadataRepository
+import com.miyagi.shashin.util.ImageProcessingUtils
 import com.miyagi.shashin.util.TextUtils
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
@@ -24,7 +27,9 @@ class TimelineController {
     @Autowired
     private val metadataRepository: MetadataRepository? = null
     @Value("\${app.sidecar.path}")
-    private var relativeSidecarDir: String? = null
+    private lateinit var relativeSidecarDir: String
+    @Value("\${app.api.version}")
+    private var apiVersion: String? = null
     @Value("\${app.mediaDir}")
     lateinit var rootMediaDir: String
 
@@ -64,23 +69,94 @@ class TimelineController {
             // Update DB
             metadataRepository?.save(metadataObj.get())
             // Update MD file
-            val rootPath = FileSystemResource("").file.absolutePath
-            val sidecarDir = rootPath + relativeSidecarDir
-            val metadataDirectory = sidecarDir.dropLast(1) + "/metadata"
-            val rootDirFile = File(rootMediaDir)
-            val photoFile = File(metadataObj.get().getPath())
-            var fileRootDir: String = (photoFile.parent).lowercase().replace((rootDirFile.canonicalPath).lowercase(), "")
-            fileRootDir = fileRootDir.replace('\\', '/')
-            val metadataFileStr = metadataDirectory + fileRootDir + "/" + photoFile.name + ".yaml"
-            val mdFile = File(metadataFileStr)
-            val yamlFactory: YAMLFactory = YAMLFactory.builder()
-                .enable(YAMLGenerator.Feature.MINIMIZE_QUOTES)
-                .disable(YAMLGenerator.Feature.SPLIT_LINES)
-                .build()
-            val om = ObjectMapper(yamlFactory)
-            om.writeValue(mdFile, metadataObj.get());
+            val imageProcessingUtils = ImageProcessingUtils(apiVersion)
+            imageProcessingUtils.saveMetadata(metadataObj.get(), relativeSidecarDir, rootMediaDir)
             return "{\"status\":\"success\",\"msg\":\"Saved\"}"
         }
+        return "{\"status\":\"fail\",\"msg\":\"Not Saved\"}"
+    }
+
+    @RequestMapping(value = ["/timeline/update/batch"], method = [RequestMethod.POST], produces = ["application/json"])
+    @ResponseBody
+    fun updateBatchMetadata(@RequestBody requestBody: JsonNode): String? {
+        val mapper = ObjectMapper()
+        val batchMetadataMap = mapper.convertValue(requestBody, object : TypeReference<Map<String, Any>>() {})
+
+        var idArray: Array<String>? = null
+        var dayTaken: Int? = null
+        var monthTaken: Int? = null
+        var yearTaken: Int? = null
+        var lat: String? = null
+        var lng: String? = null
+
+        for ((k, v) in batchMetadataMap) {
+            if (v != "") {
+
+                when (k) {
+                    "batchMetadataIds" -> {
+                        idArray = mapper.readValue(v.toString(), Array<String>::class.java)
+                    }
+                    "dayTakenBatchData" -> {
+                        dayTaken = v.toString().toInt()
+                    }
+                    "monthTakenBatchData" -> {
+                        monthTaken = v.toString().toInt()
+                    }
+                    "yearTakenBatchData" -> {
+                        yearTaken = v.toString().toInt()
+                    }
+                    "latBatchData" -> {
+                        lat = v.toString()
+                    }
+                    "lngBatchData" -> {
+                        lng = v.toString()
+                    }
+                }
+            }
+        }
+
+        if (!idArray.isNullOrEmpty()) {
+            val metadataList: ArrayList<Metadata> = ArrayList()
+
+            for (id in idArray) {
+                val metadataObj: Optional<Metadata?>? = metadataRepository?.findById(id)
+
+                if (metadataObj != null) {
+                    val metadata = metadataObj.get()
+
+                    if (dayTaken != null) {
+                        metadata.setDay(dayTaken)
+                    }
+                    if (monthTaken != null) {
+                        metadata.setMonth(monthTaken)
+                    }
+                    if (yearTaken != null) {
+                        metadata.setYear(yearTaken)
+                    }
+                    if (lat != null) {
+                        metadata.setLat(lat)
+                    }
+                    if (lng != null) {
+                        metadata.setLng(lng)
+                    }
+
+                    metadataList.add(metadata)
+                }
+            }
+
+            if (metadataList.isNotEmpty()) {
+                // Update DB
+                metadataRepository?.saveAll(metadataList)
+
+                // Update MD file
+                val imageProcessingUtils = ImageProcessingUtils(apiVersion)
+                for (metadata in metadataList) {
+                    imageProcessingUtils.saveMetadata(metadata, relativeSidecarDir, rootMediaDir)
+                }
+                return "{\"status\":\"success\",\"msg\":\"Saved\"}"
+            }
+        }
+
         return "{\"status\":\"fail\",\"msg\":\"Not Saved\"}"
     }
 }
