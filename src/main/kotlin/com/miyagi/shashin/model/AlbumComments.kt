2ae5f6c8361bdd40274b37aa9baabdfe3ac397f9
Michael Yagi <michaeltyagi@gmail.com>
2021-08-20 11:26:46 -0700

    View to dynamically update comments

---
+++ src/main/kotlin/com/miyagi/shashin/model/AlbumComments.kt
@@ -0,0 +1,46 @@
+package com.miyagi.shashin.model
+
+interface AlbumComments {
+    fun getComment(): String?
+    fun getCommentId(): Int?
+    fun getAlbumId(): Int?
+    fun getUserId(): Int?
+    fun getUsername(): String?
+
+//    private var userId: Int?
+//    private var username: String? = null
+//    private var albumId: Int? = null
+//    private var isShared: Int? = null
+//
+//    constructor(userId: Int?,username: String?,albumId: Int?,isShared: Int?) {
+//        this.userId = userId
+//        this.username = username
+//        this.albumId = albumId
+//        this.isShared = isShared
+//    }
+//
+//    fun getUserId(): Int? {
+//        return this.userId
+//    }
+//    fun setUserId(userId: Int?) {
+//        this.userId = userId
+//    }
+//    fun getUsername(): String? {
+//        return this.username
+//    }
+//    fun setUsername(username: String?) {
+//        this.username = username
+//    }
+//    fun getAlbumId(): Int? {
+//        return this.albumId
+//    }
+//    fun setAlbumId(albumId: Int?) {
+//        this.albumId = userId
+//    }
+//    fun getIsShared(): Int? {
+//        return this.isShared
+//    }
+//    fun setIsShared(isShared: Int?) {
+//        this.isShared = isShared
+//    }
+}
