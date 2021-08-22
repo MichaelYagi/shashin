1e15e2c065b59b9aa3fb08d7638902cb89d267c3
Michael Yagi <michaeltyagi@gmail.com>
2021-08-22 14:31:30 -0700

    Search keywords or filename

---
+++ src/main/kotlin/com/miyagi/shashin/controller/SearchController.kt
@@ -0,0 +1,80 @@
+package com.miyagi.shashin.controller
+
+import com.miyagi.shashin.model.Metadata
+import com.miyagi.shashin.repository.SearchRepository
+import com.miyagi.shashin.repository.UserRepository
+import com.miyagi.shashin.util.TextUtils
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.MediaType
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.ui.set
+import org.springframework.util.MultiValueMap
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.servlet.mvc.support.RedirectAttributes
+import javax.servlet.http.HttpServletRequest
+
+
+@Controller
+class SearchController {
+
+    @Value("\${app.role.admin}")
+    private var adminRole: String? = null
+
+    @Value("\${app.role.user}")
+    private var userRole: String? = null
+
+    @Autowired
+    private val searchRepository: SearchRepository? = null
+
+    @Autowired
+    private val userRepository: UserRepository? = null
+
+    @GetMapping("/search")
+    fun getSearch(model: Model, request: HttpServletRequest): String {
+        model["searchTerm"] = ""
+        model["metadataSearchList"] = ""
+
+        val searchTerm = request.getParameter("searchTerm")
+        if (!searchTerm.isNullOrBlank()) {
+            model["searchTerm"] = searchTerm
+            if (model.getAttribute("authority").toString() == adminRole) {
+                val metadataList = searchRepository?.findMetadataBySearchTerm(searchTerm)
+                model["metadataSearchList"] = metadataList as MutableIterable<Metadata>
+            } else if (model.getAttribute("authority").toString() == userRole) {
+                val currentUserObj = userRepository?.findByUsername(model.getAttribute("username").toString())
+                if (currentUserObj != null) {
+                    val metadataList = searchRepository?.findMetadataBySearchTermAndUserId(searchTerm, currentUserObj.getId())
+                    model["metadataSearchList"] = metadataList as MutableIterable<Metadata>
+                }
+            }
+        }
+
+        val module = "search"
+        model["data"] = ""
+        model["activePage"] = module
+        model["activeSidebar"] = module
+        model["titleDescriptor"] = TextUtils.capitalized(module)
+        return module
+    }
+
+    @RequestMapping(value = ["/search"], method = [RequestMethod.POST], consumes = [MediaType.APPLICATION_FORM_URLENCODED_VALUE])
+    fun postSearch(model: Model, redirectAttributes: RedirectAttributes, @RequestBody formData: MultiValueMap<String, String>): String {
+        model["searchTerm"] = ""
+        if (formData.containsKey("appSearchInput")) {
+            val searchTerm: String = java.lang.String.valueOf(formData.getFirst("appSearchInput"))
+            redirectAttributes.addAttribute("searchTerm", searchTerm);
+        }
+
+        val module = "search"
+        model["data"] = ""
+        model["activePage"] = module
+        model["activeSidebar"] = module
+        model["titleDescriptor"] = TextUtils.capitalized(module)
+        return "redirect:/"+module
+    }
+}
---
+++ src/main/kotlin/com/miyagi/shashin/repository/SearchRepository.kt
@@ -0,0 +1,13 @@
+package com.miyagi.shashin.repository
+
+import com.miyagi.shashin.model.Metadata
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.query.Param
+
+interface SearchRepository : CrudRepository<Metadata?, String?> {
+    @Query("SELECT * FROM metadata WHERE keywords LIKE %:searchTerm% OR file_name LIKE %:searchTerm%", nativeQuery = true)
+    fun findMetadataBySearchTerm(@Param("searchTerm") searchTerm: String): MutableIterable<Metadata>
+    @Query("SELECT DISTINCT m.* FROM metadata m INNER JOIN albumphoto a ON m.id = a.metadata_id INNER JOIN useralbum ua ON ua.album_id = a.album_id WHERE (m.keywords LIKE %:searchTerm% OR m.file_name LIKE %:searchTerm%) AND ua.user_id = :userId", nativeQuery = true)
+    fun findMetadataBySearchTermAndUserId(@Param("searchTerm") searchTerm: String, @Param("userId") userId: Int): MutableIterable<Metadata>
+}
