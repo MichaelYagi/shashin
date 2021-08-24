4dad995adb049b4d75c300a10c819b4ff278f520
Michael Yagi <michaeltyagi@gmail.com>
2021-08-23 20:30:35 -0700

    Initial setup for pagination

--- src/main/kotlin/com/miyagi/shashin/repository/MetadataRepository.kt
+++ src/main/kotlin/com/miyagi/shashin/repository/MetadataRepository.kt
@@ -11,4 +11,7 @@ import org.springframework.data.repository.query.Param
 interface MetadataRepository : PagingAndSortingRepository<Metadata?, String?> {
    @Query("SELECT DISTINCT * FROM metadata m LEFT JOIN albumphoto ap ON m.id = ap.metadata_id LEFT JOIN useralbum ua ON ap.album_id = ua.album_id LEFT JOIN album a ON a.id = ua.album_id WHERE ua.user_id = :userId", nativeQuery = true)
    fun findByAlbumMetadataByUserId(@Param("userId") userId: Int): MutableIterable<Metadata>
+   @Query("SELECT * FROM metadata ORDER BY year DESC, month DESC, day DESC LIMIT :offset, :limit", nativeQuery = true)
+   fun findAllByOffsetAndLimit(@Param("offset") offset: Int, @Param("limit") limit: Int): MutableIterable<Metadata>
+
 }
