e54cfe0d2716514a6fdafdc03d31de23ed196068
Michael Yagi <michaeltyagi@gmail.com>
2021-08-25 06:13:12 -0700

    Setup tests

---
+++ src/test/kotlin/com/miyagi/shashin/controller/TimelineControllerTests.kt
@@ -0,0 +1,17 @@
+package com.miyagi.shashin.controller
+
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+
+@SpringBootTest
+class TimelineControllerTests {
+    @Autowired
+    private val timelineController: TimelineController? = null
+
+    @Test
+    fun testTimeline() {
+
+    }
+}
