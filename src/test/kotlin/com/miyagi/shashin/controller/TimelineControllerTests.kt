b8b80785787b577742cc0c1c43726653c1565000
Michael Yagi <michaeltyagi@gmail.com>
2021-08-25 15:07:11 -0700

    Added basic tests

--- src/test/kotlin/com/miyagi/shashin/controller/TimelineControllerTests.kt
+++ src/test/kotlin/com/miyagi/shashin/controller/TimelineControllerTests.kt
@@ -3,17 +3,22 @@ package com.miyagi.shashin.controller
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
-
 import org.springframework.boot.web.server.LocalServerPort
-
-
+import org.springframework.security.test.context.support.WithUserDetails
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc(addFilters=false)
 class TimelineControllerTests {
-    @Autowired
     private val timelineController: TimelineController? = null
 
     @LocalServerPort
@@ -22,9 +27,11 @@ class TimelineControllerTests {
     @Autowired
     private val restTemplate: TestRestTemplate? = null
 
+    private var mvc: MockMvc? = null
+
     @Test
     @Throws(Exception::class)
-    fun timelineShouldReturnLoginIfNotLoggedIn() {
+    fun timelineShouldReturnLogin() {
         assertThat(
             this.restTemplate?.getForObject(
                 "http://localhost:" + port.toString() + "/timeline",
@@ -32,4 +39,10 @@ class TimelineControllerTests {
             )
         ).contains("Please Login")
     }
+
+    @Test
+    @Throws(Exception::class)
+    fun timelineShouldReturnTimeline() {
+    }
+
 }
