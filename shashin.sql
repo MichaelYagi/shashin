5a3579aea062fd63f484101e6e62758dacafce51
Michael Yagi <michaeltyagi@gmail.com>
2021-09-02 17:39:20 -0700

    Added additional settings form elements

--- shashin.sql
+++ shashin.sql
@@ -6,49 +6,59 @@ CREATE TABLE `hibernate_sequence` (
 DROP TABLE IF EXISTS `metadata`;
 CREATE TABLE `metadata` (
     `id` VARCHAR(36) NOT NULL DEFAULT '00000000-00000000-00000000-00000000',
-    `timeZone` VARCHAR(20) DEFAULT NULL,
-    `takenAt` DATETIME DEFAULT NULL,
+    `time_zone` VARCHAR(20) DEFAULT NULL,
+    `taken_at` DATETIME DEFAULT NULL,
     `year` INT(4) DEFAULT NULL,
     `month` INT(2) DEFAULT NULL,
     `day` INT(2) DEFAULT NULL,
     `path` VARCHAR(255) DEFAULT NULL,
-    `fileName` VARCHAR(255) DEFAULT NULL,
-    `thumbnailPathSmall` VARCHAR(255) DEFAULT NULL,
-    `thumbnailUrlSmall` VARCHAR (255) DEFAULT NULL,
-    `thumbnailPathCentered` VARCHAR(255) DEFAULT NULL,
-    `thumbnailUrlCentered` VARCHAR (255) DEFAULT NULL,
-    `thumbnailUrlOriginal` VARCHAR (255) DEFAULT NULL,
-    `mapMarkerPath` VARCHAR (255) DEFAULT NULL,
-    `mapMarkerUrl` VARCHAR (255) DEFAULT NULL,
-    `videoUrl` VARCHAR (255) DEFAULT NULL,
+    `file_name` VARCHAR(255) DEFAULT NULL,
+    `thumbnail_path_small` VARCHAR(255) DEFAULT NULL,
+    `thumbnail_url_small` VARCHAR (255) DEFAULT NULL,
+    `thumbnail_path_centered` VARCHAR(255) DEFAULT NULL,
+    `thumbnail_url_centered` VARCHAR (255) DEFAULT NULL,
+    `thumbnail_url_original` VARCHAR (255) DEFAULT NULL,
+    `map_marker_path` VARCHAR (255) DEFAULT NULL,
+    `map_marker_url` VARCHAR (255) DEFAULT NULL,
+    `video_url` VARCHAR (255) DEFAULT NULL,
     `type` VARCHAR(20) DEFAULT NULL,
     `lat` VARCHAR(20) DEFAULT NULL,
     `lng` VARCHAR(20) DEFAULT NULL,
-    `placeName` VARCHAR(255) DEFAULT NULL,
+    `place_name` VARCHAR(255) DEFAULT NULL,
     `camera` VARCHAR(255) DEFAULT NULL,
     `lens` VARCHAR(255) DEFAULT NULL,
     `quality` VARCHAR(20) DEFAULT NULL,
     `iso` INT(10) DEFAULT NULL,
     `exposure` VARCHAR(20) DEFAULT NULL,
-    `fNumber` REAL(10) DEFAULT NULL,
-    `focalLength` REAL(10) DEFAULT NULL,
+    `f_number` REAL(10) DEFAULT NULL,
+    `focal_length` REAL(10) DEFAULT NULL,
     `keywords` VARCHAR(500) DEFAULT NULL,
-    `recognitionLabelId` INT(10) DEFAULT NULL,
-    `recognitionConfidence` VARCHAR(36) NOT NULL DEFAULT '100.0', -- 100.0 to be labelled, -1.0 not a face
-    `createdAt` DATETIME DEFAULT NULL,
-    `modifiedAt` DATETIME DEFAULT NULL,
-    `lastAccessedAt` DATETIME DEFAULT NULL,
+    `recognition_label_id` INT(10) DEFAULT NULL,
+    `recognition_confidence` VARCHAR(36) NOT NULL DEFAULT '100.0', -- 100.0 to be labelled, -1.0 not a face
+    `created_at` DATETIME DEFAULT NULL,
+    `modified_at` DATETIME DEFAULT NULL,
+    `last_accessed_at` DATETIME DEFAULT NULL,
     PRIMARY KEY (`id`),
-    FOREIGN KEY (`recognitionLabelId`) REFERENCES recognitionlabel(`id`)
+    FOREIGN KEY (`recognition_label_id`) REFERENCES recognitionlabel(`id`)
 );
 
 DROP TABLE IF EXISTS `recognitionlabel`;
 CREATE TABLE `recognitionlabel` (
     `id` INTEGER PRIMARY KEY AUTOINCREMENT,
     `name` VARCHAR(50) NOT NULL,
-    `createdAt` DATETIME DEFAULT NULL,
-    `modifiedAt` DATETIME DEFAULT NULL
+    `created_at` DATETIME DEFAULT NULL,
+    `modified_at` DATETIME DEFAULT NULL
 );
+
+DROP TABLE IF EXISTS `settings`;
+CREATE TABLE `settings` (
+    `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+    `recognition_confidence_threshold` VARCHAR(36) NOT NULL DEFAULT '0.6',
+    `query_limit` INTEGER NOT NULL DEFAULT 20,
+    `created_at` DATETIME DEFAULT NULL,
+    `modified_at` DATETIME DEFAULT NULL
+);
+INSERT INTO `settings` (`recognition_confidence_threshold`,`query_limit`) VALUES('0.6',20);
 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
@@ -56,98 +66,98 @@ CREATE TABLE `user` (
     `username` VARCHAR(50) NOT NULL,
     `password` VARCHAR(50) NOT NULL,
     `authority` VARCHAR(50) NOT NULL,
-    `loggedIn` BOOLEAN DEFAULT NULL CHECK (`loggedIn` IN (0, 1)),
-    `createdAt` DATETIME DEFAULT NULL,
-    `modifiedAt` DATETIME DEFAULT NULL
+    `logged_in` BOOLEAN DEFAULT NULL CHECK (`logged_in` IN (0, 1)),
+    `created_at` DATETIME DEFAULT NULL,
+    `modified_at` DATETIME DEFAULT NULL
 );
 
 DROP TABLE IF EXISTS `mediadir`;
 CREATE TABLE `mediadir` (
     `id` INTEGER PRIMARY KEY AUTOINCREMENT,
     `directory` VARCHAR(150) NOT NULL,
-    `createdAt` DATETIME DEFAULT NULL,
-    `modifiedAt` DATETIME DEFAULT NULL
+    `created_at` DATETIME DEFAULT NULL,
+    `modified_at` DATETIME DEFAULT NULL
 );
 
 DROP TABLE IF EXISTS `album`;
 CREATE TABLE `album` (
     `id` INTEGER PRIMARY KEY AUTOINCREMENT,
     `name` VARCHAR(100) NOT NULL UNIQUE,
-    `coverUrl` VARCHAR(255) DEFAULT NULL,
-    `shareUrl` VARCHAR(255) DEFAULT NULL,
-    `createdAt` DATETIME DEFAULT NULL,
-    `modifiedAt` DATETIME DEFAULT NULL
+    `cover_url` VARCHAR(255) DEFAULT NULL,
+    `share_url` VARCHAR(255) DEFAULT NULL,
+    `created_at` DATETIME DEFAULT NULL,
+    `modified_at` DATETIME DEFAULT NULL
 );
 
 DROP TABLE IF EXISTS `useralbum`;
 CREATE TABLE `useralbum` (
     `id` INTEGER PRIMARY KEY AUTOINCREMENT,
-    `userId` INTEGER,
-    `albumId` INTEGER,
-    `createdAt` DATETIME DEFAULT NULL,
-    `modifiedAt` DATETIME DEFAULT NULL,
-    UNIQUE(`userId`,`albumId`) ON CONFLICT IGNORE,
-    FOREIGN KEY (`albumId`) REFERENCES album(`id`),
-    FOREIGN KEY (`userId`) REFERENCES user(`id`)
+    `user_id` INTEGER,
+    `album_id` INTEGER,
+    `created_at` DATETIME DEFAULT NULL,
+    `modified_at` DATETIME DEFAULT NULL,
+    UNIQUE(`user_id`,`album_id`) ON CONFLICT IGNORE,
+    FOREIGN KEY (`album_id`) REFERENCES album(`id`),
+    FOREIGN KEY (`user_id`) REFERENCES user(`id`)
 );
 
 DROP TABLE IF EXISTS `albumphoto`;
 CREATE TABLE `albumphoto` (
     `id` INTEGER PRIMARY KEY AUTOINCREMENT,
-    `metadataId` VARCHAR(36),
-    `albumId` INTEGER,
-    `createdAt` DATETIME DEFAULT NULL,
-    `modifiedAt` DATETIME DEFAULT NULL,
-    UNIQUE(`metadataId`,`albumId`) ON CONFLICT IGNORE,
-    FOREIGN KEY (`albumId`) REFERENCES album(`id`),
-    FOREIGN KEY (`metadataId`) REFERENCES metadata(`id`)
+    `metadata_id` VARCHAR(36),
+    `album_id` INTEGER,
+    `created_at` DATETIME DEFAULT NULL,
+    `modified_at` DATETIME DEFAULT NULL,
+    UNIQUE(`metadata_id`,`album_id`) ON CONFLICT IGNORE,
+    FOREIGN KEY (`album_id`) REFERENCES album(`id`),
+    FOREIGN KEY (`metadata_id`) REFERENCES metadata(`id`)
 );
 
 DROP TABLE IF EXISTS `favorite`;
 CREATE TABLE `favorite` (
      `id` INTEGER PRIMARY KEY AUTOINCREMENT,
-     `userId` INTEGER,
-     `metadataId` INTEGER,
-     `createdAt` DATETIME DEFAULT NULL,
-     `modifiedAt` DATETIME DEFAULT NULL,
-     UNIQUE(`metadataId`,`userId`) ON CONFLICT IGNORE,
-     FOREIGN KEY (`metadataId`) REFERENCES metadata(`id`),
-     FOREIGN KEY (`userId`) REFERENCES user(`id`)
+     `user_id` INTEGER,
+     `metadata_id` INTEGER,
+     `created_at` DATETIME DEFAULT NULL,
+     `modified_at` DATETIME DEFAULT NULL,
+     UNIQUE(`metadata_id`,`user_id`) ON CONFLICT IGNORE,
+     FOREIGN KEY (`metadata_id`) REFERENCES metadata(`id`),
+     FOREIGN KEY (`user_id`) REFERENCES user(`id`)
 );
 
 DROP TABLE IF EXISTS `comment`;
 CREATE TABLE `comment` (
     `id` INTEGER PRIMARY KEY AUTOINCREMENT,
     `comment` VARCHAR,
-    `userId` INT,
-    `createdAt` DATETIME DEFAULT NULL,
-    `modifiedAt` DATETIME DEFAULT NULL
+    `user_id` INT,
+    `created_at` DATETIME DEFAULT NULL,
+    `modified_at` DATETIME DEFAULT NULL
 );
 
 DROP TABLE IF EXISTS `albumcomment`;
 CREATE TABLE `albumcomment` (
     `id` INTEGER PRIMARY KEY AUTOINCREMENT,
-    `albumId` INT,
-    `commentId` INT,
-    `createdAt` DATETIME DEFAULT NULL,
-    `modifiedAt` DATETIME DEFAULT NULL,
-    UNIQUE(`commentId`,`albumId`) ON CONFLICT IGNORE,
-    FOREIGN KEY (`commentId`) REFERENCES comment(`id`),
-    FOREIGN KEY (`albumId`) REFERENCES album(`id`)
+    `album_id` INT,
+    `comment_id` INT,
+    `created_at` DATETIME DEFAULT NULL,
+    `modified_at` DATETIME DEFAULT NULL,
+    UNIQUE(`comment_id`,`album_id`) ON CONFLICT IGNORE,
+    FOREIGN KEY (`comment_id`) REFERENCES comment(`id`),
+    FOREIGN KEY (`album_id`) REFERENCES album(`id`)
 );
 
 DROP TABLE IF EXISTS `albumphotocomment`;
 CREATE TABLE `albumphotocomment` (
     `id` INTEGER PRIMARY KEY AUTOINCREMENT,
-    `metadataId` INT,
-    `albumId` INT,
-    `commentId` INT,
-    `createdAt` DATETIME DEFAULT NULL,
-    `modifiedAt` DATETIME DEFAULT NULL,
-    UNIQUE(`commentId`,`metadataId`,`albumId`) ON CONFLICT IGNORE,
-    FOREIGN KEY (`commentId`) REFERENCES comment(`id`),
-    FOREIGN KEY (`metadataId`) REFERENCES metadata(`id`),
-    FOREIGN KEY (`albumId`) REFERENCES album(`id`)
+    `metadata_id` INT,
+    `album_id` INT,
+    `comment_id` INT,
+    `created_at` DATETIME DEFAULT NULL,
+    `modified_at` DATETIME DEFAULT NULL,
+    UNIQUE(`comment_id`,`metadata_id`,`album_id`) ON CONFLICT IGNORE,
+    FOREIGN KEY (`comment_id`) REFERENCES comment(`id`),
+    FOREIGN KEY (`metadata_id`) REFERENCES metadata(`id`),
+    FOREIGN KEY (`album_id`) REFERENCES album(`id`)
 );
 
 INSERT INTO `hibernate_sequence` VALUES (362);
---
+++ src/main/kotlin/com/miyagi/shashin/repository/SettingsRepository.kt
@@ -0,0 +1,8 @@
+package com.miyagi.shashin.repository
+
+import com.miyagi.shashin.model.Settings
+import org.springframework.data.repository.CrudRepository
+
+interface SettingsRepository : CrudRepository<Settings?, Int?> {
+    fun findFirstByOrderByIdAsc(): Settings
+}
