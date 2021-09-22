774e90f5f37e6106b315c6c7a90d4a886a8f43c5
Michael Yagi <michaeltyagi@gmail.com>
2021-09-21 19:42:45 -0700

    Created list of timezone offset

--- src/main/kotlin/com/miyagi/shashin/util/TextUtils.kt
+++ src/main/kotlin/com/miyagi/shashin/util/TextUtils.kt
@@ -1,15 +1,19 @@
 package com.miyagi.shashin.util
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import net.iakovlev.timeshape.TimeZoneEngine
 import org.springframework.stereotype.Component
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.net.URL
 import java.text.SimpleDateFormat
+import java.time.Instant
+import java.time.ZoneId
+import java.time.ZoneOffset
 import java.util.*
 import java.util.logging.Level
 import java.util.logging.Logger
+import java.util.stream.Collectors
+import kotlin.collections.HashMap
 
 
 @Component
@@ -107,6 +111,49 @@ class TextUtils {
             }
 
             return buildPlace
+        }
+
+        fun timeOffsets(): List<String> {
+
+            val offsets: List<String> = listOf(
+                "−12:00",
+                "−11:00",
+                "−10:00",
+                "−09:30",
+                "−09:00",
+                "−07:00",
+                "−06:00",
+                "−05:00",
+                "−04:30",
+                "−04:00",
+                "−03:00",
+                "−02:00",
+                "−01:00",
+                "±00:00",
+                "+01:00",
+                "+02:00",
+                "+03:00",
+                "+04:00",
+                "+04:30",
+                "+05:00",
+                "+05:30",
+                "+05:45",
+                "+06:00",
+                "+06:30",
+                "+07:00",
+                "+08:00",
+                "+08:45",
+                "+09:00",
+                "+09:30",
+                "+10:00",
+                "+11:00",
+                "+11:30",
+                "+12:00",
+                "+13:00",
+                "+14:00"
+            )
+
+            return offsets
         }
 
         private fun readUrl(urlString: String): String? {
