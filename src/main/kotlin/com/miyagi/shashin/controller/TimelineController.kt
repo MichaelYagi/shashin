286727cf83bed8ebd9e03d31ab82ac395e167673
Michael Yagi <michaeltyagi@gmail.com>
2021-09-26 11:04:13 -0700

    Timeline message tweak

--- src/main/kotlin/com/miyagi/shashin/controller/TimelineController.kt
+++ src/main/kotlin/com/miyagi/shashin/controller/TimelineController.kt
@@ -92,7 +92,11 @@ class TimelineController {
     private fun buildTimelineData(model: Model,mediaTypeFilter: String,page: Int,isInitialRequest: Boolean): MutableMap<String, Any?> {
         val response = mutableMapOf<String, Any?>()
 
-        response["message"] = "There are no photos. Please setup directories in Settings and scan ."
+        var mediaType = "photo"
+        if (mediaTypeFilter == "video") {
+            mediaType = mediaTypeFilter
+        }
+        response["message"] = "There are no "+mediaType+"s. Please setup directories in Settings and scan."
         response["metadataList"] = ""
         response["favorites"] = ""
         response["albumList"] = ""
