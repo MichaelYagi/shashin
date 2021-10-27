c8514379bffdbe4fa950ca6f3ce42c1f17406eca
Michael Yagi <michaeltyagi@gmail.com>
2021-10-26 23:08:04 -0700

    Order album photos by date descending

--- src/main/kotlin/com/miyagi/shashin/repository/AlbumPhotoRepository.kt
+++ src/main/kotlin/com/miyagi/shashin/repository/AlbumPhotoRepository.kt
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Repository
 interface AlbumPhotoRepository : CrudRepository<AlbumPhoto?, Int?> {
     fun countByMetadataIdAndAlbumId(metdataId: String?, albumId: Int?): Int?
     fun countByAlbumId(albumId: Int?): Int?
-    @Query("SELECT * FROM albumphoto WHERE album_id = :albumId LIMIT :offset, :limit", nativeQuery = true)
+    @Query("SELECT DISTINCT ap.* FROM albumphoto ap, metadata m WHERE ap.album_id = :albumId  AND ap.metadata_id = m.id ORDER BY m.year DESC, m.month DESC, m.day DESC, m.time DESC LIMIT :offset, :limit", nativeQuery = true)
     fun findAllByAlbumIdAndOffsetAndLimit(@Param("albumId") albumId: Int, @Param("offset") offset: Int, @Param("limit") limit: Int): MutableIterable<AlbumPhoto?>?
     fun findFirstByOrderByIdAsc(): AlbumPhoto?
     fun findAlbumPhotoByMetadataId(metadataId: String?): MutableIterable<AlbumPhoto?>?
