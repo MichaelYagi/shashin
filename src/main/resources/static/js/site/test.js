3ee7f6cab4b26cba109c159d0a0d4b65d06400b5
Michael Yagi <michaeltyagi@gmail.com>
2021-10-28 09:56:11 -0700

    Adding a few RN components

--- src/main/resources/static/js/site/test.js
+++ src/main/resources/static/js/site/test.js
@@ -5,3 +5,7 @@ function sum(a, b) {
 if (typeof module !== 'undefined') {
     module.exports = sum;
 }
+
+// const myelement = <h1>I Love JSX!</h1>;
+//
+// ReactDOM.render(myelement, document.getElementById('rnRoot'));
