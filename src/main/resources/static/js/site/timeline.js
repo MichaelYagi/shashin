74590fee3eddf6def5a9ad9678862463675a550c
Michael Yagi <michaeltyagi@gmail.com>
2021-11-05 10:08:29 -0700

    Adjusted timeline query limit

--- src/main/resources/static/js/site/timeline.js
+++ src/main/resources/static/js/site/timeline.js
@@ -375,7 +375,7 @@
     timelineSettings.renderThumbnails = async function(id,mediaTypeFilter) {
         let deferred = new $.Deferred();
 
-        let queryLimit = 4;
+        let queryLimit = 3;
 
         // Depth of results in section of page above and below anchor
         let depthDown = queryLimit;
