b0ff25387d551563694f2ee9635153cb219f98a4
Michael Yagi <michaeltyagi@gmail.com>
2021-11-23 06:48:57 -0800

    Cleaning up code

--- src/main/resources/static/js/site/app.js
+++ src/main/resources/static/js/site/app.js
@@ -66,8 +66,6 @@ $.fn.serializeObject = function() {
     shashin.infiniteScrollGallery = null;
     shashin.lg = null;
     shashin.ajaxRetries = 3;
-    shashin.timelineQueryLimit = 1;
-    shashin.scrollDirection = "down";
 
     function fixContentHeight(){
         const viewHeight = $(window).height();
