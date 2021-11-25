b0ca84ea99d30c4e7d3f4f8c952c225d54b570d8
Michael Yagi <michaeltyagi@gmail.com>
2021-11-25 07:02:22 -0800

    Introduced Util class

---
+++ src/main/resources/static/js/site/util.js
@@ -0,0 +1,36 @@
+class Util {
+    static isChrome() {
+        const isChromium = window.chrome;
+        const winNav = window.navigator;
+        const vendorName = winNav.vendor;
+        const isOpera = typeof window.opr !== "undefined";
+        const isIEedge = winNav.userAgent.indexOf("Edg") > -1;
+        const isIOSChrome = winNav.userAgent.match("CriOS");
+
+        let isChrome = false;
+        if (isIOSChrome ||
+            (isChromium !== null &&
+                typeof isChromium !== "undefined" &&
+                vendorName === "Google Inc." &&
+                isOpera === false &&
+                isIEedge === false)
+        ) {
+            isChrome = true;
+        }
+
+        return isChrome;
+    }
+
+    static isSafari() {
+        const isSafari = navigator.vendor && navigator.vendor.indexOf('Apple') > -1 &&
+            navigator.userAgent &&
+            navigator.userAgent.indexOf('CriOS') == -1 &&
+            navigator.userAgent.indexOf('FxiOS') == -1;
+
+        return isSafari;
+    }
+
+    static isFirefox() {
+        return navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
+    }
+}
