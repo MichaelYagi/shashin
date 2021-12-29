2f1f40aae614c2a599a07acd78dc994b9e284e0d
Michael Yagi <michaeltyagi@gmail.com>
2021-12-28 19:06:01 -0800

    Send array of metadata ids in batch operations

--- src/main/kotlin/com/miyagi/shashin/controller/TimelineController.kt
+++ src/main/kotlin/com/miyagi/shashin/controller/TimelineController.kt
@@ -923,14 +923,22 @@ class TimelineController {
                         }
                     }
 
-                    if (isObject) {
-                        // Process tagged people
-                        val recognitionLabelPhotos = recognitionLabelPhotoRepository?.findByMetadataId(metadata.getId())
-                        if (recognitionLabelPhotos != null) {
-                            for (recognitionLabelPhoto in recognitionLabelPhotos) {
-                                recognitionLabelPhotoRepository?.delete(recognitionLabelPhoto)
-                            }
+                    // Process tagged people
+                    val recognitionLabelPhotos = recognitionLabelPhotoRepository?.findByMetadataId(metadata.getId())
+                    if (recognitionLabelPhotos != null) {
+                        for (recognitionLabelPhoto in recognitionLabelPhotos) {
+                            recognitionLabelPhotoRepository?.delete(recognitionLabelPhoto)
                         }
+                    }
+
+                    if (isObject) {
+//                        // Process tagged people
+//                        val recognitionLabelPhotos = recognitionLabelPhotoRepository?.findByMetadataId(metadata.getId())
+//                        if (recognitionLabelPhotos != null) {
+//                            for (recognitionLabelPhoto in recognitionLabelPhotos) {
+//                                recognitionLabelPhotoRepository?.delete(recognitionLabelPhoto)
+//                            }
+//                        }
 
                         recognitionLabelPhotoRepository?.deleteByMetadataId(metadata.getId())
                         val recognitionLabelRecord = recognitionLabelRepository?.findByNameIgnoreCase("object")
@@ -950,13 +958,13 @@ class TimelineController {
                         recognitionLabelPhotoObj.setConfidence("-0.1")
                         recognitionLabelPhotoRepository?.save(recognitionLabelPhotoObj)
                     } else if (recognitionLabelNames != null && recognitionLabelNames.toString().trim() != "") {
-                        // Process tagged people
-                        val recognitionLabelPhotos = recognitionLabelPhotoRepository?.findByMetadataId(metadata.getId())
-                        if (recognitionLabelPhotos != null) {
-                            for (recognitionLabelPhoto in recognitionLabelPhotos) {
-                                recognitionLabelPhotoRepository?.delete(recognitionLabelPhoto)
-                            }
-                        }
+//                        // Process tagged people
+//                        val recognitionLabelPhotos = recognitionLabelPhotoRepository?.findByMetadataId(metadata.getId())
+//                        if (recognitionLabelPhotos != null) {
+//                            for (recognitionLabelPhoto in recognitionLabelPhotos) {
+//                                recognitionLabelPhotoRepository?.delete(recognitionLabelPhoto)
+//                            }
+//                        }
 
                         val recognitionLabelArray = recognitionLabelNames.toString().split(",")
                         if (recognitionLabelArray.count() > 0) {
