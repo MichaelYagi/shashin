bfbf606eca896d3e33fb173940411d3f12e98c73
Michael Yagi <michaeltyagi@gmail.com>
2021-12-31 18:18:23 -0800

    Added album selenium tests

--- src/test/kotlin/com/miyagi/shashin/TimelineSeleniumTest.kt
+++ src/test/kotlin/com/miyagi/shashin/TimelineSeleniumTest.kt
@@ -106,6 +106,7 @@ class TimelineSeleniumTest: BaseSeleniumTests() {
 
         // Check if UUID present
         this.driver!!.get("http://localhost:$port/timeline")
+        println(this.driver?.pageSource)
         val scrollContainer = this.driver!!.findElement(By.id("infinite-scroll-gallery"))
         WebDriverWait(this.driver, 30).until(ExpectedConditions.visibilityOfAllElements(scrollContainer.findElement(By.xpath("./span[1]"))))
         val spanContainerEl = scrollContainer.findElement(By.xpath("./span[1]"))
