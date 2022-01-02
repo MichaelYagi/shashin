b878b0596dd16a7e3d5599b006a0d796188616b2
Michael Yagi <michaeltyagi@gmail.com>
2022-01-02 09:14:36 -0800

    Organized tests

--- src/test/com/miyagi/shashin/e2e/TimelineSeleniumTest.kt
+++ src/test/com/miyagi/shashin/e2e/TimelineSeleniumTest.kt
@@ -1,150 +1,171 @@
-package com.miyagi.shashin
+package com.miyagi.shashin.e2e
 
-//import com.miyagi.shashin.model.User
-//import com.miyagi.shashin.repository.UserRepository
-//import org.junit.jupiter.api.Assertions
-//import org.junit.jupiter.api.BeforeEach
-//import org.junit.jupiter.api.Test
-//import org.openqa.selenium.By
-//import org.openqa.selenium.support.ui.ExpectedConditions
-//import org.openqa.selenium.support.ui.WebDriverWait
-//import org.springframework.beans.factory.annotation.Autowired
-//import org.springframework.boot.test.context.SpringBootTest
-//import org.springframework.boot.web.server.LocalServerPort
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-//import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity
-//import org.springframework.test.context.ActiveProfiles
-//import org.springframework.test.web.servlet.MockMvc
-//import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder
-//import org.springframework.test.web.servlet.setup.MockMvcBuilders
-//import org.springframework.web.context.WebApplicationContext
-//import java.io.File
-//import java.net.URL
-//import java.util.*
-//
-//
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-//@ActiveProfiles("test")
-//class TimelineSeleniumTest: BaseSeleniumTests() {
-//
-//    private var adminId: Int? = null
-//    private var userId: Int? = null
-//    private var mockMvc: MockMvc? = null
-//
-//    @LocalServerPort
-//    private val port = 0
-//
-//    @Autowired
-//    private val context: WebApplicationContext? = null
-//
-//    @Autowired
-//    private val userRepository: UserRepository? = null
-//
-//
-//    private var bcrypt = BCryptPasswordEncoder()
-//
-//    @BeforeEach
-//    fun setup() {
-//        val adminObj = User()
-//        adminObj.setUsername("testadmin")
-//        var encodedPassword: String = bcrypt.encode("testadmin")
-//        adminObj.setPassword(encodedPassword)
-//        adminObj.setAuthority("ROLE_ADMIN")
-//        adminObj.setIsAllowed(true)
-//        userRepository?.save(adminObj)
-//        adminId = adminObj.getId()
-//
-//        val userObj = User()
-//        userObj.setUsername("testuser")
-//        encodedPassword = bcrypt.encode("testuser")
-//        userObj.setPassword(encodedPassword)
-//        userObj.setAuthority("ROLE_USER")
-//        userObj.setIsAllowed(true)
-//        userRepository?.save(userObj)
-//        userId = userObj.getId()
-//
-//        mockMvc = MockMvcBuilders
-//            .webAppContextSetup(context!!)
-//            .apply<DefaultMockMvcBuilder>(springSecurity())
-//            .build()
-//
-//        this.driver?.get("http://localhost:$port/users/login")
-//        //println(this.driver?.pageSource)
-//        val username = this.driver!!.findElement(By.id("username"))
-//        val password = this.driver!!.findElement(By.id("password"))
-//        val login = this.driver!!.findElement(By.tagName("button"))
-//        username.sendKeys("testadmin")
-//        password.sendKeys("testadmin")
-//        login.click()
-//    }
-//
-//    @Test
-//    @Throws(Exception::class)
-//    fun shouldUploadPhotoAndViewInTimeline() {
-//        Assertions.assertEquals("http://localhost:$port/timeline", this.driver!!.currentUrl)
-//
-//        this.driver!!.get("http://localhost:$port/settings")
-//
-//        // Delete and start fresh
-//        val deleteContent = this.driver!!.findElement(By.id("deleteContent"))
-//        deleteContent.click()
-//        WebDriverWait(this.driver, 30).until(ExpectedConditions.visibilityOfElementLocated(By.id("deleteAllContent")))
-//        val deleteAllContent = this.driver!!.findElement(By.id("deleteAllContent"))
-//        deleteAllContent.click()
-//        WebDriverWait(this.driver, 30).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[contains(text(),'Success!')]")))
-//        this.driver!!.get("http://localhost:$port/settings")
-//
-//        // Get test image data and populate in settings
-//        val classLoader = javaClass.classLoader
-//        val testImageUrl: URL = classLoader.getResource("testscreen.jpg")
-//        val testImageFile = File(testImageUrl.file)
-//        val mediaDirTextArea = this.driver!!.findElement(By.id("mediaDirTextArea"))
-//        mediaDirTextArea.sendKeys(testImageFile.parent)
-//
-//        val saveSettings = this.driver!!.findElement(By.id("saveSettings"))
-//        saveSettings.click()
-//        WebDriverWait(this.driver, 30).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[contains(text(),'Settings saved')]")))
-//
-//        // Scan new image
-//        this.driver!!.get("http://localhost:$port/settings/scan");
-//        val scanPhotos = this.driver!!.findElement(By.id("scanPhotos"))
-//        scanPhotos.click()
-//
-//        val wait = WebDriverWait(this.driver, 30)
-//        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[contains(text(),'Scan Complete')]")))
-//
-//        // Check if UUID present
-//        this.driver!!.get("http://localhost:$port/timeline")
-//        // println(this.driver?.pageSource)
-//        val scrollContainer = this.driver!!.findElement(By.id("infinite-scroll-gallery"))
-//        WebDriverWait(this.driver, 30).until(ExpectedConditions.visibilityOfAllElements(scrollContainer.findElement(By.xpath("./span[1]"))))
-//        val spanContainerEl = scrollContainer.findElement(By.xpath("./span[1]"))
-//        val dateId = spanContainerEl.getAttribute("id").substringAfter("container_")
-//        val parentEl = this.driver!!.findElement(By.id("row$dateId"))
-//        val childEl = parentEl.findElement(By.xpath("./div[1]"))
-//        val imageId = childEl.getAttribute("id")
-//        val metadataId = imageId.substringAfter("photoThumbnailContainer")
-//
-//        Assertions.assertTrue(isUUID(metadataId))
-//
-//        // Check image src
-//        WebDriverWait(this.driver, 30).until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("img[src^='/api/v1/thumbnails']")))
-////        println(this.driver?.pageSource)
-//        val imageEl = this.driver!!.findElement(By.id("image$metadataId"))
-//        val imageSrc = imageEl.getAttribute("src")
-//
-//        Assertions.assertTrue(imageSrc.contains("testscreen.jpg"))
-//    }
-//
-//    private fun isUUID(someUUID: String): Boolean {
-//        val isUuid = try {
-//            val uuid = UUID.fromString(someUUID)
-//            true
-//        } catch (exception: IllegalArgumentException) {
-//            //handle the case where string is not valid UUID
-//            false
-//        }
-//
-//        return isUuid
-//    }
-//}
+import com.miyagi.shashin.model.User
+import com.miyagi.shashin.repository.MetadataRepository
+import com.miyagi.shashin.repository.UserRepository
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.openqa.selenium.By
+import org.openqa.selenium.support.ui.ExpectedConditions
+import org.openqa.selenium.support.ui.WebDriverWait
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.web.server.LocalServerPort
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.web.context.WebApplicationContext
+import java.io.File
+import java.net.URL
+import java.util.*
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+class TimelineSeleniumTest: BaseSeleniumTests() {
+
+    private var metadataId: String? = null
+    private var adminId: Int? = null
+    private var userId: Int? = null
+    private var mockMvc: MockMvc? = null
+
+    @LocalServerPort
+    private val port = 0
+
+    @Autowired
+    private val context: WebApplicationContext? = null
+
+    @Autowired
+    private val metadataRepository: MetadataRepository? = null
+
+    @Autowired
+    private val userRepository: UserRepository? = null
+
+
+    private var bcrypt = BCryptPasswordEncoder()
+
+    @BeforeEach
+    fun setup() {
+        val adminObj = User()
+        adminObj.setUsername("testadmin")
+        var encodedPassword: String = bcrypt.encode("testadmin")
+        adminObj.setPassword(encodedPassword)
+        adminObj.setAuthority("ROLE_ADMIN")
+        adminObj.setIsAllowed(true)
+        userRepository?.save(adminObj)
+        adminId = adminObj.getId()
+
+        val userObj = User()
+        userObj.setUsername("testuser")
+        encodedPassword = bcrypt.encode("testuser")
+        userObj.setPassword(encodedPassword)
+        userObj.setAuthority("ROLE_USER")
+        userObj.setIsAllowed(true)
+        userRepository?.save(userObj)
+        userId = userObj.getId()
+
+        mockMvc = MockMvcBuilders
+            .webAppContextSetup(context!!)
+            .apply<DefaultMockMvcBuilder>(springSecurity())
+            .build()
+
+        this.driver?.get("http://localhost:$port/users/login")
+        //println(this.driver?.pageSource)
+        val username = this.driver!!.findElement(By.id("username"))
+        val password = this.driver!!.findElement(By.id("password"))
+        val login = this.driver!!.findElement(By.tagName("button"))
+        username.sendKeys("testadmin")
+        password.sendKeys("testadmin")
+        login.click()
+    }
+
+    @AfterEach
+    fun teardown() {
+        // Delete metadata
+        val metadataObj = metadataRepository?.findById(metadataId!!)
+        val thumbnailPathSmall = metadataObj?.get()?.getThumbnailPathSmall()
+        val thumbnailDir = File(thumbnailPathSmall!!).parent
+        val metadataDir = thumbnailDir.replace("thumbnails","metadata")
+        val thumbnailDirFile = File(thumbnailDir)
+        val metadataDirFile = File(metadataDir)
+//        println(thumbnailDirFile.absolutePath)
+//        println(metadataDirFile.absolutePath)
+        thumbnailDirFile.deleteRecursively()
+        metadataDirFile.deleteRecursively()
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun shouldUploadPhotoAndViewInTimeline() {
+        Assertions.assertEquals("http://localhost:$port/timeline", this.driver!!.currentUrl)
+
+        this.driver!!.get("http://localhost:$port/settings")
+
+        // Delete and start fresh
+        val deleteContent = this.driver!!.findElement(By.id("deleteContent"))
+        deleteContent.click()
+        WebDriverWait(this.driver, 30).until(ExpectedConditions.visibilityOfElementLocated(By.id("deleteAllContent")))
+        val deleteAllContent = this.driver!!.findElement(By.id("deleteAllContent"))
+        deleteAllContent.click()
+        WebDriverWait(this.driver, 30).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[contains(text(),'Success!')]")))
+        this.driver!!.get("http://localhost:$port/settings")
+
+        // Get test image data and populate in settings
+        val classLoader = javaClass.classLoader
+        val testImageUrl: URL = classLoader.getResource("testscreen.jpg")
+        val testImageFile = File(testImageUrl.file)
+        val mediaDirTextArea = this.driver!!.findElement(By.id("mediaDirTextArea"))
+        mediaDirTextArea.sendKeys(testImageFile.parent)
+
+        val saveSettings = this.driver!!.findElement(By.id("saveSettings"))
+        saveSettings.click()
+        WebDriverWait(this.driver, 30).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[contains(text(),'Settings saved')]")))
+
+        // Scan new image
+        this.driver!!.get("http://localhost:$port/settings/scan");
+        val scanPhotos = this.driver!!.findElement(By.id("scanPhotos"))
+        scanPhotos.click()
+
+        val wait = WebDriverWait(this.driver, 30)
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[contains(text(),'Scan Complete')]")))
+
+        // Check if UUID present
+        this.driver!!.get("http://localhost:$port/timeline")
+        // println(this.driver?.pageSource)
+        val scrollContainer = this.driver!!.findElement(By.id("infinite-scroll-gallery"))
+        WebDriverWait(this.driver, 30).until(ExpectedConditions.visibilityOfAllElements(scrollContainer.findElement(By.xpath("./span[1]"))))
+        val spanContainerEl = scrollContainer.findElement(By.xpath("./span[1]"))
+        val dateId = spanContainerEl.getAttribute("id").substringAfter("container_")
+        val parentEl = this.driver!!.findElement(By.id("row$dateId"))
+        val childEl = parentEl.findElement(By.xpath("./div[1]"))
+        val imageId = childEl.getAttribute("id")
+        metadataId = imageId.substringAfter("photoThumbnailContainer")
+
+        Assertions.assertTrue(isUUID(metadataId!!))
+
+        // Check image src
+        WebDriverWait(this.driver, 30).until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("img[src^='/api/v1/thumbnails']")))
+//        println(this.driver?.pageSource)
+        val imageEl = this.driver!!.findElement(By.id("image$metadataId"))
+        val imageSrc = imageEl.getAttribute("src")
+
+        Assertions.assertTrue(imageSrc.contains("testscreen.jpg"))
+    }
+
+    private fun isUUID(someUUID: String): Boolean {
+        val isUuid = try {
+            val uuid = UUID.fromString(someUUID)
+            true
+        } catch (exception: IllegalArgumentException) {
+            //handle the case where string is not valid UUID
+            false
+        }
+
+        return isUuid
+    }
+}
