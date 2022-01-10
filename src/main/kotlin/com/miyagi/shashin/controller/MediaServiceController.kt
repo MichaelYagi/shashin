9d4a081f8ac4aa387b8cda8a5f48f9340d352520
Michael Yagi <michaeltyagi@gmail.com>
2022-01-09 20:27:24 -0800

    Return blank page if image dne

--- src/main/kotlin/com/miyagi/shashin/controller/MediaServiceController.kt
+++ src/main/kotlin/com/miyagi/shashin/controller/MediaServiceController.kt
@@ -39,93 +39,119 @@ class MediaServiceController {
     @RequestMapping(value = ["/api/v1/video/{metadataId}"], method = [RequestMethod.GET], produces = ["video/mp4","video/3gpp","video/mpeg","video/ogg","video/quicktime","video/webm"])
     @ResponseBody
     fun getVideo(response: HttpServletResponse?, @PathVariable metadataId: String): FileSystemResource? {
-        val metadataObj = metadataRepository.findById(metadataId)
-        var path = metadataObj.get().getPath()!!
-        val metadata = metadataObj.get()
+        val metadataCount = metadataRepository.countMetadataById(metadataId)
+        if (metadataCount > 0) {
+            val metadataObj = metadataRepository.findById(metadataId)
+            var path = metadataObj.get().getPath()!!
+            val metadata = metadataObj.get()
 
-        if (metadata.getType() != null &&
-            (metadata.getType()!!.lowercase().contains("mp4") || metadata.getType()!!.lowercase().contains("quicktime")) &&
-            (
-                (metadata.getCompressionType() == null && metadata.getExpectedExtension() != null && metadata.getExpectedExtension()!!.lowercase() == "mov" && File(metadata.getPath()!!).extension.lowercase() == "mov") ||
-                (metadata.getCompressionType() != null && metadata.getCompressionType()!!.lowercase() != "h.264")
-            )
-        ) {
-            logger.log(Level.INFO, "Converting video "+metadata.getPath()+" to h.264.")
-            /* Step 1. Declaring source file and Target file */
-            val source = File(path)
+            if (metadata.getType() != null &&
+                (metadata.getType()!!.lowercase().contains("mp4") || metadata.getType()!!.lowercase()
+                    .contains("quicktime")) &&
+                (
+                        (metadata.getCompressionType() == null && metadata.getExpectedExtension() != null && metadata.getExpectedExtension()!!
+                            .lowercase() == "mov" && File(metadata.getPath()!!).extension.lowercase() == "mov") ||
+                                (metadata.getCompressionType() != null && metadata.getCompressionType()!!
+                                    .lowercase() != "h.264")
+                        )
+            ) {
+                logger.log(Level.INFO, "Converting video " + metadata.getPath() + " to h.264.")
+                /* Step 1. Declaring source file and Target file */
+                val source = File(path)
 
-            val tempFilePath = System.getProperty("java.io.tmpdir")+"/temp.mp4"
-            if (Files.exists(Paths.get(tempFilePath))) {
-                val tempFile = File(tempFilePath)
-                tempFile.delete()
+                val tempFilePath = System.getProperty("java.io.tmpdir") + "/temp.mp4"
+                if (Files.exists(Paths.get(tempFilePath))) {
+                    val tempFile = File(tempFilePath)
+                    tempFile.delete()
+                }
+                val target = File(tempFilePath)
+
+                /* Step 2. Set Audio Attributes for conversion*/
+                val audio = AudioAttributes()
+                audio.setCodec("aac")
+                audio.setBitRate(64000)
+                audio.setChannels(2)
+                audio.setSamplingRate(44100)
+
+                /* Step 3. Set Video Attributes for conversion*/
+                val video = VideoAttributes()
+                video.setCodec("h264")
+                video.setX264Profile(X264_PROFILE.BASELINE)
+                // More the frames and higher bitrate means more quality and size,
+                // keep it low based on devices like mobile
+                // Here 160 kbps video is 160000
+                //video.setBitRate(160000)
+                //video.setFrameRate(15)
+                val width = if (metadata.getOriginalImageWidth() == null) 209 else metadata.getOriginalImageWidth()!!
+                val height = if (metadata.getOriginalImageHeight() == null) 209 else metadata.getOriginalImageHeight()!!
+                video.setSize(VideoSize(width, height))
+
+                /* Step 4. Set Encoding Attributes*/
+                val attrs = EncodingAttributes()
+                attrs.setOutputFormat("mp4")
+                attrs.setAudioAttributes(audio)
+                attrs.setVideoAttributes(video)
+
+                /* Step 5. Do the Encoding*/
+                try {
+                    val encoder = Encoder()
+                    encoder.encode(MultimediaObject(source), target, attrs)
+                    path = target.path
+                } catch (e: Exception) {
+                    /*Handle here the video failure*/
+                    logger.log(
+                        Level.SEVERE,
+                        "Could not convert video " + metadata.getPath() + " to h.264: " + e.message
+                    )
+
+                }
+
+                if (target.exists()) {
+                    target.delete()
+                }
             }
-            val target = File(tempFilePath)
-
-            /* Step 2. Set Audio Attributes for conversion*/
-            val audio = AudioAttributes()
-            audio.setCodec("aac")
-            audio.setBitRate(64000)
-            audio.setChannels(2)
-            audio.setSamplingRate(44100)
-
-            /* Step 3. Set Video Attributes for conversion*/
-            val video = VideoAttributes()
-            video.setCodec("h264")
-            video.setX264Profile(X264_PROFILE.BASELINE)
-            // More the frames and higher bitrate means more quality and size,
-            // keep it low based on devices like mobile
-            // Here 160 kbps video is 160000
-            //video.setBitRate(160000)
-            //video.setFrameRate(15)
-            val width = if (metadata.getOriginalImageWidth() == null) 209 else metadata.getOriginalImageWidth()!!
-            val height = if (metadata.getOriginalImageHeight() == null) 209 else metadata.getOriginalImageHeight()!!
-            video.setSize(VideoSize(width, height))
-
-            /* Step 4. Set Encoding Attributes*/
-            val attrs = EncodingAttributes()
-            attrs.setOutputFormat("mp4")
-            attrs.setAudioAttributes(audio)
-            attrs.setVideoAttributes(video)
-
-            /* Step 5. Do the Encoding*/
-            try {
-                val encoder = Encoder()
-                encoder.encode(MultimediaObject(source), target, attrs)
-                path = target.path
-            } catch (e: Exception) {
-                /*Handle here the video failure*/
-                logger.log(Level.SEVERE, "Could not convert video "+metadata.getPath()+" to h.264: "+e.message)
-
-            }
-
-            if (target.exists()) {
-                target.delete()
-            }
+            return FileSystemResource(path)
+        } else {
+            return null
         }
-        return FileSystemResource(path)
     }
 
     @RequestMapping(value = ["/api/v1/video/{metadataId}/download"], method = [RequestMethod.GET], produces = ["video/mp4","video/3gpp","video/mpeg","video/ogg","video/quicktime","video/webm"])
     @ResponseBody
     fun getVideoDownload(response: HttpServletResponse?, @PathVariable metadataId: String): FileSystemResource? {
-        val metadataObj = metadataRepository.findById(metadataId)
-        val path = metadataObj.get().getPath()!!
-        return FileSystemResource(path)
+        val metadataCount = metadataRepository.countMetadataById(metadataId)
+        return if (metadataCount > 0) {
+            val metadataObj = metadataRepository.findById(metadataId)
+            val path = metadataObj.get().getPath()!!
+            FileSystemResource(path)
+        } else {
+            null
+        }
     }
 
     @RequestMapping(value = ["/api/v1/image/{metadataId}"], method = [RequestMethod.GET], produces = ["image/apng","image/avif","image/gif","image/jpeg","image/png","image/svg+xml","image/svg+xml","image/webp"])
     @ResponseBody
     fun getImage(response: HttpServletResponse?, @PathVariable metadataId: String): FileSystemResource? {
-        val metadataObj = metadataRepository.findById(metadataId)
-        val path = metadataObj.get().getPath()!!
-        return FileSystemResource(path)
+        val metadataCount = metadataRepository.countMetadataById(metadataId)
+        return if (metadataCount > 0) {
+            val metadataObj = metadataRepository.findById(metadataId)
+            val path = metadataObj.get().getPath()!!
+            FileSystemResource(path)
+        } else {
+            null
+        }
     }
 
     @RequestMapping(value = ["/api/v1/audio/{metadataId}"], method = [RequestMethod.GET], produces = ["audio/3gpp","audio/aac","audio/flac","audio/mpeg","audio/mp3","audio/mp4","audio/ogg","audio/wav","audio/webm"])
     @ResponseBody
     fun getAudio(response: HttpServletResponse?, @PathVariable metadataId: String): FileSystemResource? {
-        val metadataObj = metadataRepository.findById(metadataId)
-        val path = metadataObj.get().getPath()!!
-        return FileSystemResource(path)
+        val metadataCount = metadataRepository.countMetadataById(metadataId)
+        return if (metadataCount > 0) {
+            val metadataObj = metadataRepository.findById(metadataId)
+            val path = metadataObj.get().getPath()!!
+            FileSystemResource(path)
+        } else {
+            null
+        }
     }
 }
