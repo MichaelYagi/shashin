3270d8873364ef0a54c5cd2fcbec82fb0ee3b85e
Michael Yagi <michaeltyagi@gmail.com>
2022-02-13 12:43:41 -0800

    Using SpringBoot cache in timeline queries

--- src/main/kotlin/com/miyagi/shashin/repository/MetadataRepository.kt
+++ src/main/kotlin/com/miyagi/shashin/repository/MetadataRepository.kt
@@ -1,7 +1,6 @@
 package com.miyagi.shashin.repository
 
 import com.miyagi.shashin.model.*
-import org.springframework.cache.annotation.Cacheable
 import org.springframework.data.domain.Sort
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
@@ -57,7 +56,6 @@ interface MetadataRepository : PagingAndSortingRepository<Metadata?, String?> {
    @Query("SELECT * FROM metadata WHERE hidden = false ORDER BY added_at DESC LIMIT :offset, :limit", nativeQuery = true)
    fun findRecentByOffsetAndLimit(@Param("offset") offset: Int, @Param("limit") limit: Int): MutableIterable<Metadata>
 
-   @Cacheable("metadataByDate")
    fun findAllByYearAndMonthAndDayAndHiddenEqualsOrderByYearDescMonthDescDayDescTimeDesc(year: Int?, month: Int?, day: Int?, hidden: Boolean?): MutableIterable<Metadata>
 
    @Query("SELECT * FROM metadata WHERE type LIKE %:type% AND hidden = false ORDER BY year DESC, month DESC, day DESC, time DESC", nativeQuery = true)
@@ -66,7 +64,6 @@ interface MetadataRepository : PagingAndSortingRepository<Metadata?, String?> {
    @Query("SELECT * FROM metadata WHERE type LIKE %:type% AND hidden = false ORDER BY year DESC, month DESC, day DESC, time DESC LIMIT :offset, :limit", nativeQuery = true)
    fun findAllByTypeOffsetAndLimit(@Param("type") type: String,@Param("offset") offset: Int, @Param("limit") limit: Int): MutableIterable<Metadata>
 
-   @Cacheable("metadataByDateAndMediaType")
    @Query("SELECT * FROM metadata WHERE type LIKE %:type% AND year = :year AND month = :month AND day = :day AND hidden = false ORDER BY year DESC, month DESC, day DESC, time DESC", nativeQuery = true)
    fun findAllByTypeAndYearAndMonthAndDay(@Param("type") type: String,@Param("year") year: Int?,@Param("month") month: Int?,@Param("day") day: Int?): MutableIterable<Metadata>
 
