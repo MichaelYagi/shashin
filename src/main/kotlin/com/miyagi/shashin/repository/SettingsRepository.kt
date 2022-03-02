33d0acc9c675943fea117342eecf6a8ca3f59ad9
Michael Yagi <michaeltyagi@gmail.com>
2022-03-02 12:44:50 -0800

    Added caching to tineline date calls

--- src/main/kotlin/com/miyagi/shashin/repository/SettingsRepository.kt
+++ src/main/kotlin/com/miyagi/shashin/repository/SettingsRepository.kt
@@ -3,7 +3,9 @@ package com.miyagi.shashin.repository
 import com.miyagi.shashin.model.Settings
 import org.springframework.cache.annotation.Cacheable
 import org.springframework.data.repository.CrudRepository
+import javax.transaction.Transactional
 
+@Transactional
 interface SettingsRepository : CrudRepository<Settings?, Int?> {
     @Cacheable("firstSettingQuery")
     fun findFirstByOrderByIdAsc(): Settings?
