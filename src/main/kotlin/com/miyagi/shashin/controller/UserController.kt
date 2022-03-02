03f3646bcbab682355a4b3076f82c19076be9b62
Michael Yagi <michaeltyagi@gmail.com>
2022-03-02 10:09:14 -0800

    Added caching to tineline date calls

--- src/main/kotlin/com/miyagi/shashin/controller/UserController.kt
+++ src/main/kotlin/com/miyagi/shashin/controller/UserController.kt
@@ -207,7 +207,6 @@ class UserController {
         return module
     }
 
-    @CacheEvict(value = ["firstSettingQuery"], allEntries = true)
     @GetMapping("/users/login")
     fun getLoginUser(model: Model, @RequestParam(name="error",required=false) error: String?, @RequestParam(name="msg",required=false) message: String?): String {
         val module = "login"
