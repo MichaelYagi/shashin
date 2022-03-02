d49f6eb39c4f88e18f6567c66f77bf8675f34e47
Michael Yagi <michaeltyagi@gmail.com>
2022-03-02 15:45:06 -0800

    Added data loader

---
+++ src/main/kotlin/com/miyagi/shashin/component/DataLoader.kt
@@ -0,0 +1,30 @@
+package com.miyagi.shashin.component
+
+import com.miyagi.shashin.model.Settings
+import com.miyagi.shashin.repository.SettingsRepository
+import com.miyagi.shashin.util.TextUtils
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.ApplicationArguments
+import org.springframework.boot.ApplicationRunner
+import org.springframework.stereotype.Component
+
+@Component
+class DataLoader @Autowired constructor(private val settingsRepository: SettingsRepository) : ApplicationRunner {
+    override fun run(args: ApplicationArguments) {
+        val settings = settingsRepository.findFirstByOrderByIdAsc()
+        if (settings == null) {
+            val settingsObj = Settings()
+            settingsObj.setQueryLimit(20)
+            settingsObj.setMatchScanLimit(50)
+            settingsObj.setTrainingDataLimit(100)
+            settingsObj.setNotificationLimit(20)
+            settingsObj.setSearchHistoryLimit(15)
+            settingsObj.setPort("6624")
+            settingsObj.setScanAutomatically(false)
+            settingsObj.setRecognitionConfidenceThreshold("0.6")
+            settingsObj.setCreatedAt(TextUtils.getCurrentTimestamp())
+            settingsObj.setModifiedAt(TextUtils.getCurrentTimestamp())
+            settingsRepository.save(settingsObj)
+        }
+    }
+}
