1110833d7d28a67c3797ee28b8d046050995627a
Michael Yagi <michaeltyagi@gmail.com>
2023-04-10 09:48:28 -0700

    Removing config values to properties file

--- src/main/kotlin/com/miyagi/shashin/component/DataLoader.kt
+++ src/main/kotlin/com/miyagi/shashin/component/DataLoader.kt
@@ -4,6 +4,7 @@ import com.miyagi.shashin.model.Settings
 import com.miyagi.shashin.repository.SettingsRepository
 import com.miyagi.shashin.util.TextUtils
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
 import org.springframework.cache.annotation.CacheEvict
@@ -11,20 +12,41 @@ import org.springframework.stereotype.Component
 
 @Component
 class DataLoader @Autowired constructor(private val settingsRepository: SettingsRepository) : ApplicationRunner {
-    
+
+    @Value("\${app.config.default.querylimit}")
+    private var queryLimitProperty: Int = 20
+
+    @Value("\${app.config.default.matchscanlimit}")
+    private var matchScanLimitProperty: Int = 50
+
+    @Value("\${app.config.default.trainingdatalimit}")
+    private var trainingDataLimitProperty: Int = 100
+
+    @Value("\${app.config.default.notificationlimit}")
+    private var notificationLimitProperty: Int = 20
+
+    @Value("\${app.config.default.searchhistorylimit}")
+    private var searchHistoryLimitProperty: Int = 15
+
+    @Value("\${app.config.default.recognitionConfidenceThreshold}")
+    private lateinit var recognitionConfidenceThresholdProperty: String
+
+    @Value("\${server.port}")
+    private lateinit var portProperty: String
+
     @CacheEvict(value = ["firstSettingQuery"], allEntries = true)
     override fun run(args: ApplicationArguments) {
         val settings = settingsRepository.findFirstByOrderByIdAsc()
         if (settings == null) {
             val settingsObj = Settings()
-            settingsObj.setQueryLimit(20)
-            settingsObj.setMatchScanLimit(50)
-            settingsObj.setTrainingDataLimit(100)
-            settingsObj.setNotificationLimit(20)
-            settingsObj.setSearchHistoryLimit(15)
-            settingsObj.setPort("6624")
+            settingsObj.setQueryLimit(queryLimitProperty)
+            settingsObj.setMatchScanLimit(matchScanLimitProperty)
+            settingsObj.setTrainingDataLimit(trainingDataLimitProperty)
+            settingsObj.setNotificationLimit(notificationLimitProperty)
+            settingsObj.setSearchHistoryLimit(searchHistoryLimitProperty)
+            settingsObj.setPort(portProperty)
             settingsObj.setScanAutomatically(false)
-            settingsObj.setRecognitionConfidenceThreshold("0.6")
+            settingsObj.setRecognitionConfidenceThreshold(recognitionConfidenceThresholdProperty)
             settingsObj.setCreatedAt(TextUtils.getCurrentTimestamp())
             settingsObj.setModifiedAt(TextUtils.getCurrentTimestamp())
             settingsRepository.save(settingsObj)
