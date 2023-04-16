947cb0e710a9c9b843b13e62e3de5f8f46d6831c
Michael Yagi <michaeltyagi@gmail.com>
2023-04-15 20:33:41 -0700

    Added ability to download albums

--- src/main/resources/static/js/site/util.js
+++ src/main/resources/static/js/site/util.js
@@ -483,6 +483,17 @@ class Util {
         }
     }
 
+    static formatBytes(bytes, decimals = 2) {
+        if (!+bytes) return '0 Bytes'
+
+        const k = 1024
+        const dm = decimals < 0 ? 0 : decimals
+        const sizes = ['Bytes', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB']
+
+        const i = Math.floor(Math.log(bytes) / Math.log(k))
+
+        return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`
+    }
     static getDateGalleryHeight(id) {
         if ($("#br" + id).length === 0 && $("#row" + id).length === 0 && $("#amp_" + id).length === 0 && $("#tail_" + id).length === 0 && $("#" + id).length === 0) {
             return 0;
