cf2429901546623deff4c519ab26b338e0354b6c
Michael Yagi <michaeltyagi@gmail.com>
2023-06-14 09:27:19 -0700

    Added exclude field in UI and DB

--- src/main/kotlin/com/miyagi/shashin/repository/MediaDirectoryRepository.kt
+++ src/main/kotlin/com/miyagi/shashin/repository/MediaDirectoryRepository.kt
@@ -2,10 +2,18 @@ package com.miyagi.shashin.repository
 
 import com.miyagi.shashin.model.MediaDirectory
 import com.miyagi.shashin.model.User
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
+import javax.transaction.Transactional
 
 @Repository
 interface MediaDirectoryRepository : CrudRepository<MediaDirectory?, Int?> {
-    fun findByDirectory(directory: String?): MediaDirectory?
+    fun findByExclude(exclude: Boolean): MutableIterable<MediaDirectory?>
+
+    @Query("SELECT * FROM mediadir WHERE exclude = :exclude AND directory= :directory", nativeQuery = true)
+    fun findByExcludeIsAndDirectory(exclude: Boolean, directory: String?): MediaDirectory?
+
+    @Transactional
+    fun deleteByExclude(exclude: Boolean): Long?
 }
