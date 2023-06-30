907ad924c6ab7f9fdcdcfe29f7218f7f4bc59367
Michael Yagi <michaeltyagi@gmail.com>
2023-06-29 20:43:14 -0700

    Improved commenting UX

--- src/main/resources/static/js/site/templates/modal.js
+++ src/main/resources/static/js/site/templates/modal.js
@@ -33,7 +33,7 @@ class ModalTemplates {
                         <textarea class="form-control" id="commentText${metadata.id}" rows="2"></textarea>
                         <button type="button" class="btn btn-primary" id="saveCommentMetadata${metadata.id}">Save</button>
                         <button type="button" class="btn btn-primary" id="updateCommentMetadata${metadata.id}">Update</button>
-                        <button type="button" class="btn btn-secondary" id="dismissModalCommentMetadata${metadata.id}" data-bs-dismiss="modal">Cancel</button>
+                        <button type="button" class="btn btn-secondary" id="dismissModalCommentMetadata${metadata.id}" data-bs-dismiss="modal">Close</button>
                         <button type="button" class="btn btn-secondary" id="cancelEditCommentMetadata${metadata.id}">Cancel</button>
                     </div>
                 </div>
