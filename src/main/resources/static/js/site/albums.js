835937a58d954230f086cc236ed84a6b87bf73b0
Michael Yagi <michaeltyagi@gmail.com>
2023-07-10 13:33:07 -0700

    Minor code refactoring

--- src/main/resources/static/js/site/albums.js
+++ src/main/resources/static/js/site/albums.js
@@ -225,7 +225,7 @@ class Albums {
                             html += '<p id="commentcontent' + comments.commentId + '">' + comments.comment + '</p>';
                             html += '<small>';
 
-                            if (comments.userProfile !== "" && comments.userProfile !== null) {
+                            if (comments.userProfile !== "" && comments.userProfile !== null && comments.userProfile !== "null") {
                                 html += '<img src="'+comments.userProfile+'?'+uuidv4()+'" class="me-1" style="display:inline-block;width:24px;height:24px;" />';
                             } else {
                                 html += '<span class="bi-person-circle me-1" style="font-size:1.0rem;"></span>';
