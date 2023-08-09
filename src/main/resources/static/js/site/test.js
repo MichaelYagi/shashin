33d5656116e2d0cb9286e309c8fa22f653bd43b4
Michael Yagi <michaeltyagi@gmail.com>
2023-08-08 19:50:27 -0700

    Added options to toast messages

--- src/main/resources/static/js/site/test.js
+++ src/main/resources/static/js/site/test.js
@@ -14,10 +14,10 @@ if (typeof module !== 'undefined') {
     //module.exports = sum;
 }
 
-shashin.showToastMessage("Title 1", "Message 1.");
-shashin.showToastMessage("Title 2", "Message 2", {target:"liveToast1"});
-shashin.showToastMessage("Title 3", "Message 3", {autohide: false, target:"liveToast4", icon:"bi-alt", iconColor:"#ff0000"});
-shashin.showToastMessage("Title 4", "Message 5.", {autohide: false, target:"liveToast3", icon:"bi-exclamation-triangle", iconColor:"#000000"});
+// shashin.showToastMessage("Title 1", "Message 1.");
+// shashin.showToastMessage("Title 2", "Message 2", {target:"liveToast1"});
+// shashin.showToastMessage("Title 3", "Message 3", {autohide: false, target:"liveToast4", icon:"bi-alt", iconColor:"#ff0000"});
+// shashin.showToastMessage("Title 4", "Message 5.", {autohide: false, target:"liveToast3", icon:"bi-exclamation-triangle", iconColor:"#000000"});
 
 
 
