991f58f30532221736324621b1f53020ad9787fb
Michael Yagi <michaeltyagi@gmail.com>
2023-08-08 19:46:56 -0700

    Added options to toast messages

--- src/main/resources/static/js/site/test.js
+++ src/main/resources/static/js/site/test.js
@@ -14,6 +14,13 @@ if (typeof module !== 'undefined') {
     //module.exports = sum;
 }
 
+shashin.showToastMessage("Title 1", "Message 1.");
+shashin.showToastMessage("Title 2", "Message 2", {target:"liveToast1"});
+shashin.showToastMessage("Title 3", "Message 3", {autohide: false, target:"liveToast4", icon:"bi-alt", iconColor:"#ff0000"});
+shashin.showToastMessage("Title 4", "Message 5.", {autohide: false, target:"liveToast3", icon:"bi-exclamation-triangle", iconColor:"#000000"});
+
+
+
 // const myelement = <h1>I Love JSX!</h1>;
 //
 // ReactDOM.render(myelement, document.getElementById('rnRoot'));
