e92f36750d52ff0c153e4f8433ccba911ac673af
Michael Yagi <michaeltyagi@gmail.com>
2023-09-12 17:57:40 -0700

    Added title to more icons

--- src/main/resources/static/js/site/templates/timeline.js
+++ src/main/resources/static/js/site/templates/timeline.js
@@ -128,19 +128,19 @@ class TimelineTemplates {
     static TimelineGalleryTopRightOverlay({metadata}) { return `
         ${metadata.type.indexOf("video") >= 0 ?
             `
-            <span class="overlayIconBackground"><span id="duration${metadata.id}">${(metadata.hasOwnProperty("duration") && metadata.duration !== null && metadata.duration !== "") ? metadata.duration : "0:00"}</span>&nbsp;<span id="video${metadata.id}" class="bi-camera-video overlayIcon"></span></span>
+            <span class="overlayIconBackground"><span id="duration${metadata.id}">${(metadata.hasOwnProperty("duration") && metadata.duration !== null && metadata.duration !== "") ? metadata.duration : "0:00"}</span>&nbsp;<span id="video${metadata.id}" class="bi-camera-video overlayIcon" title="video"></span></span>
             `
             :
             ` 
                 ${(metadata.originalImageWidth !== null && metadata.originalImageHeight !== null && metadata.originalImageWidth > metadata.originalImageHeight * 2) ?
                     `
-                        <span id="panorama${metadata.id}" class="bi-aspect-ratio overlayIcon overlayIconBackground"></span>
+                        <span id="panorama${metadata.id}" class="bi-aspect-ratio overlayIcon overlayIconBackground" title="panorama"></span>
                     `
                 :
                 `
                     ${metadata.expectedExtension === "gif" ?
                     `
-                    <span id="gif${metadata.id}" class="bi-layers overlayIcon overlayIconBackground"></span>
+                    <span id="gif${metadata.id}" class="bi-layers overlayIcon overlayIconBackground" title="gif"></span>
                     `
                     : ''
                 }
