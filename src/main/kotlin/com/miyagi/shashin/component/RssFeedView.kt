6910d1eb2261589e3e4edeebb028bf0415884b3c
Michael Yagi <michaeltyagi@gmail.com>
2023-09-25 08:25:18 -0700

    Added album name to RSS descriptions

--- src/main/kotlin/com/miyagi/shashin/component/RssFeedView.kt
+++ src/main/kotlin/com/miyagi/shashin/component/RssFeedView.kt
@@ -95,16 +95,17 @@ class RssFeedView : AbstractRssFeedView() {
                                         var albumName = ""
                                         if (metadata.getPlaceName() != null && metadata.getPlaceName() != "") {
                                             val placeArray = metadata.getPlaceName()!!.split(";")
-                                            place = placeArray[0]
+                                            place = placeArray[0] + " - "
                                         }
                                         if (metadata.getDescription() != null && metadata.getDescription() != "") {
-                                            metadataDescription = metadata.getDescription()!!
+                                            metadataDescription = metadata.getDescription()!! + " - "
                                         }
                                         if (album?.getName() != null && album.getName() != "") {
-                                            albumName = album.getName()!!
+                                            albumName = album.getName()!! + " - "
                                         }
                                         val descVal = "$albumName $metadataDescription $place"
-                                        description.value = descVal.trim()
+
+                                        description.value = descVal.dropLast(3).trim()
                                         entry.description = description
                                         entry.link = "$baseUrl/api/v1/image/${metadata.getId()}"
                                         entry.uri = "$baseUrl/api/v1/image/${metadata.getId()}"
