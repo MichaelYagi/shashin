34c769e64e7090a7c428bdbe5905e002a9e2e8ca
Michael Yagi <michaeltyagi@gmail.com>
2023-10-14 09:23:12 -0700

    Fixing video processing bugs

--- src/main/resources/static/js/site/util.js
+++ src/main/resources/static/js/site/util.js
@@ -226,7 +226,7 @@ class Util {
 
                 if (activePage !== "timeline") {
                     window.top.location = window.top.location;
-                } else if (metadataIdArray.length === 1 && metadataKeys.length === 1 && metadataIdArray[0] === metadataKeys[0]) {
+                } else if (metadataIdArray.length === 1 && metadataKeys.length === 1) {
                     // If single value, update fields in timeline view
                     const rescannedMetadata = metadataMap[metadataKeys[0]];
                     let takenDateUpdated = false;
