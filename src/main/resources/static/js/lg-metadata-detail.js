a654c2cbb689b4c24ab9e974e380afd25649bdb9
Michael Yagi <michaeltyagi@gmail.com>
2023-10-28 17:06:55 -0700

    Minor code refactoring

--- src/main/resources/static/js/lg-metadata-detail.js
+++ src/main/resources/static/js/lg-metadata-detail.js
@@ -42,20 +42,8 @@
                         const fn = this.settings.metadataDetailFunc;
                         let metadataId = "";
 
-                        if ($($(".thumbnail-bl")[this.core.index]).children('a').attr("tag") && this.settings.hasOwnProperty("metadataDetailFunc")) {
-                            if ($($(".thumbnail-bl")[this.core.index]).children('a').attr("tag") !== undefined) {
-                                metadataId = $($(".thumbnail-bl")[this.core.index]).children('a').attr("tag");
-                            } else if ($($(".thumbnail-bl")[this.core.index]).children('a').attr("data-metadataid") !== undefined) {
-                                metadataId = $($(".thumbnail-bl")[this.core.index]).children('a').attr("data-metadataid");
-                            }
-                        }
-
-                        if (metadataId.length === 0 && $($(".thumbnail-centered")[this.core.index]).children('a').attr("tag") && this.settings.hasOwnProperty("metadataDetailFunc")) {
-                            if ($($(".thumbnail-centered")[this.core.index]).children('a').attr("tag") !== undefined) {
-                                metadataId = $($(".thumbnail-centered")[this.core.index]).children('a').attr("tag");
-                            } else if ($($(".thumbnail-centered")[this.core.index]).children('a').attr("data-metadataid") !== undefined) {
-                                metadataId = $($(".thumbnail-centered")[this.core.index]).children('a').attr("data-metadataid");
-                            }
+                        if ($($(".thumbnail-centered")[this.core.index]).children('a').attr("data-metadataid") && this.settings.hasOwnProperty("videoThumbnailFunc")) {
+                            metadataId = $($(".thumbnail-centered")[this.core.index]).children('a').attr("data-metadataid");
                         }
 
                         if (metadataId.length === 0 && shashin) {
