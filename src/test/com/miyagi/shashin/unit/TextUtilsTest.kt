6cf8c3a592fa75d21b8965b07bbcee1aedad3d57
Michael Yagi <michaeltyagi@gmail.com>
2023-11-07 20:09:43 -0800

    Added backend metadata validation tests

--- src/test/com/miyagi/shashin/unit/TextUtilsTest.kt
+++ src/test/com/miyagi/shashin/unit/TextUtilsTest.kt
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 
-@SpringBootTest
 @ActiveProfiles("test")
 class TextUtilsTest {
 
