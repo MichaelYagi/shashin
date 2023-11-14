7b1aab0e7b22d8452e175989c21fed6c4635457e
Michael Yagi <michaeltyagi@gmail.com>
2023-11-13 18:33:01 -0800

    Fixed tab hiding

--- src/main/resources/static/js/site/app.js
+++ src/main/resources/static/js/site/app.js
@@ -452,9 +452,9 @@
 
                 const latlngValue = (metadata.hasOwnProperty("lat") && metadata.hasOwnProperty("lng") && metadata.lat != null && metadata.lng != null && metadata.lat !== "" && metadata.lng !== "") ? ($.trim(metadata.lat) + ',' + $.trim(metadata.lng)) : '';
                 $("#latlng").val(latlngValue);
-                $("#mapTabLink").show();
+                $("#mapTabNav").show();
                 if (latlngValue === "") {
-                    $("#mapTabLink").hide();
+                    $("#mapTabNav").hide();
                 }
 
                 let taggedPeopleString = "";
