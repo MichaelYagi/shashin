1ea09fdf1aae498cbfc582f8add4cc8aacd2470e
Michael Yagi <michaeltyagi@gmail.com>
2024-01-21 08:18:46 -0800

    Sync checkbox and input values

--- src/main/resources/static/js/site/applisteners.js
+++ src/main/resources/static/js/site/applisteners.js
@@ -33,6 +33,7 @@ $("#appToolsBatchEdit").on("click", function(e) {
         albumNames.push($(this).val().replace(/ +(?= )/g,'').trim());
     });
     shashin.createAutocomplete("#albumNameInput", albumNames, false);
+    shashin.validateCheckboxInputs("#albumNameInput", "albums");
 
     const peoplecheckedBoxes = $('input[name="recognitionLabel[]"]');
     const peopleNames = [];
@@ -40,6 +41,7 @@ $("#appToolsBatchEdit").on("click", function(e) {
         peopleNames.push($(this).val().replace(/ +(?= )/g,'').trim());
     });
     shashin.createAutocomplete("#tagBatchDataInput", peopleNames, false);
+    shashin.validateCheckboxInputs("#tagBatchDataInput", "recognitionLabel");
 
     $("#propBatchMetadata").modal('show');
 });
