c47a954b6dd7e73c4856ed2e1b9f73aec978c858
Michael Yagi <michaeltyagi@gmail.com>
2024-02-03 17:54:49 -0800

    Added metrics util

---
+++ src/main/kotlin/com/miyagi/shashin/util/MetricsUtil.kt
@@ -0,0 +1,69 @@
+package com.miyagi.shashin.util
+
+import org.springframework.beans.factory.annotation.Value
+import java.util.*
+import java.util.logging.Level
+import java.util.logging.Logger
+
+class MetricsUtil {
+    private var counter: Int = 0
+
+    private var elapsedTime: Long = 0
+
+    private var totalElapsedTime: Long = 0
+
+    private var startTime: Long? = null
+
+    private var endTime: Long? = null
+
+    private var module: String = ""
+
+    private var logger: Logger = Logger.getLogger(MetricsUtil::class.simpleName)
+
+    @Value("\${app.config.default.slaMS}")
+    private var slaMS: Long? = null
+
+    fun start(lmodule: String = "") {
+        module = lmodule
+        startTime = System.currentTimeMillis()
+        counter++
+    }
+
+    fun end() {
+        if (slaMS == null) {
+            slaMS = 200
+        }
+
+        endTime = System.currentTimeMillis()
+
+        var logMessage = " "
+        if (module != "") {
+            logMessage = " for $module "
+        }
+
+        if (startTime != null) {
+            elapsedTime = endTime!! - startTime!!
+            totalElapsedTime += elapsedTime
+
+            logger.log(Level.INFO, "Elapsed time${logMessage}was $elapsedTime ms.")
+
+            if (counter > 1) {
+                logger.log(Level.INFO, "$counter calls for this instance elapsed time: $totalElapsedTime ms.")
+            }
+
+            if (elapsedTime > slaMS!!) {
+                logger.log(Level.WARNING, "The elapsed time${logMessage}is over the SLA threshold of $slaMS ms.")
+            }
+
+            if (counter > 1 && totalElapsedTime > slaMS!!) {
+                logger.log(Level.WARNING, "The total elapsed time${logMessage}is over the SLA threshold of $slaMS ms.")
+            }
+
+        } else {
+            logger.log(Level.WARNING, "Start time${logMessage}not defined.")
+        }
+
+        startTime = null
+        endTime = null
+    }
+}
