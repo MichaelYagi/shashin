6183a8929d1b051c1ff679588a93e28a2c49c096
Michael Yagi <michaeltyagi@gmail.com>
2024-02-09 07:36:46 -0800

    Added download button to share albums

--- src/main/resources/static/js/site/share.js
+++ src/main/resources/static/js/site/share.js
@@ -113,7 +113,7 @@ class ShareAlbum {
         const albumId = this.albumId;
         const shareLink = this.shareLink;
 
-        $("#downloadFormContainer").html('<form method="post" action="/download/share/' + shareLink + '/album/' + albumId + '" style="display: inline-block;white-space: nowrap;"><button class="bi-download link-button-lightmode" style="font-size: 2rem;color: #0d6efd;" type="submit" id="download' + albumId + '" name="download" value="' + albumId + '" title="Download all photos"></button></form>');
+        $("#downloadFormContainer").html('<form method="post" action="/download/share/' + shareLink + '/album/' + albumId + '" style="display: inline-block;white-space: nowrap;"><button class="bi-download link-button-lightmode" style="font-size: 1.5rem;color: #0d6efd;" type="submit" id="download' + albumId + '" name="download" value="' + albumId + '" title="Download all photos"></button></form>');
 
         $("#clearMultiSelect").on("click", function() {
            shashin.clearAlbumSelection();
