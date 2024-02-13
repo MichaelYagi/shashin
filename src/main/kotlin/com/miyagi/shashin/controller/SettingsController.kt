3a194dc202425e23fc9e766580df1fcc90efaacc
Michael Yagi <michaeltyagi@gmail.com>
2024-02-13 11:15:10 -0800

    UI tweaks on share link

--- src/main/kotlin/com/miyagi/shashin/controller/SettingsController.kt
+++ src/main/kotlin/com/miyagi/shashin/controller/SettingsController.kt
@@ -619,7 +619,7 @@ class SettingsController {
                     if (successDeletion) {
                         resp["msg"] = "Success!"
                     } else {
-                        resp["msg"] = "Could not delete sidecar files."
+                        resp["msg"] = "Records deleted but could not delete sidecar files."
                     }
                     resp["status"] = ApiResponse.SUCCESS.status
                     return mapper.writeValueAsString(resp)
