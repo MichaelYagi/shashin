90cb966096edce5177132b53480c98dd255ff2fe
Michael Yagi <michaeltyagi@gmail.com>
2024-03-14 13:37:09 -0700

    Select album in map by query string

--- src/main/resources/static/js/site/map.js
+++ src/main/resources/static/js/site/map.js
@@ -42,6 +42,7 @@ async function showMap(mapdata) {
 
     // Set an initial date to 500 photos ago
     let initIndex = 500;
+    initIndex = mapdata.length-initIndex;
     if (mapdata.length < initIndex) {
         initIndex = mapdata.length;
     }
