11334b7dfe636859a4c14e069a5944eb2f593f59
Michael Yagi <michaeltyagi@gmail.com>
2024-04-02 15:38:11 -0700

    Changed initial coordinate on maps

--- src/main/resources/static/js/lg-cast-media.js
+++ src/main/resources/static/js/lg-cast-media.js
@@ -72,28 +72,43 @@
                             $("#chromecasting").addClass('bi-cast').removeClass('bi-stop-circle');
                             cjs.disconnect();
                         } else {
-                            let metadataId = "";
 
-                            let currentDynamicEl = this.settings.dynamicEl[this.core.index] && this.settings.dynamicEl[this.core.index].hasOwnProperty("args") ?
-                                this.settings.dynamicEl[this.core.index] : this.core.galleryItems[this.core.index];
+                            let index = this.core.index;
 
-                            if (currentDynamicEl.hasOwnProperty("args")) {
-                                metadataId = currentDynamicEl.args;
-                            } else if (currentDynamicEl.hasOwnProperty("metadataId")) {
-                                metadataId = currentDynamicEl.metadataId;
-                            }
+                            let currentDynamicEl = this.settings.dynamicEl[index] && this.settings.dynamicEl[index].hasOwnProperty("args") ?
+                                this.settings.dynamicEl : this.core.galleryItems;
+                            
+                            const activePage = $("#activePage").val();
+                            getMediaMetadata(currentDynamicEl, index);
 
-                            if (shashin &&
-                                /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/.test(metadataId)
-                            ) {
-                                $("#metadataId").val(metadataId);
-                                shashin.getMetadata(metadataId).then(function (metadata) {
-                                    castMetadataMedia(metadata, cjs);
+                            if (activePage === "slideshow") {
+                                this.core.LGel.on('lgBeforeSlide', (e) => {
+                                    getMediaMetadata(currentDynamicEl, e.detail.index);
                                 });
-                            } else {
-                                if (shashin) {
-                                    shashin.showToastMessage("Could not get cast", "Could not cast. Metadata ID not valid", {icon:"bi-exclamation-triangle", iconColor:"#FF0000"});
-                                }
+                            }
+                        }
+                    }
+
+                    function getMediaMetadata(currentDynamicEl, index) {
+                        let metadataId = "";
+                        let currentDynamicElIndex = currentDynamicEl[index];
+
+                        if (currentDynamicElIndex.hasOwnProperty("args")) {
+                            metadataId = currentDynamicElIndex.args;
+                        } else if (currentDynamicElIndex.hasOwnProperty("metadataId")) {
+                            metadataId = currentDynamicElIndex.metadataId;
+                        }
+
+                        if (shashin &&
+                            /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/.test(metadataId)
+                        ) {
+                            $("#metadataId").val(metadataId);
+                            shashin.getMetadata(metadataId).then(function (metadata) {
+                                castMetadataMedia(metadata, cjs);
+                            });
+                        } else {
+                            if (shashin) {
+                                shashin.showToastMessage("Could not get cast", "Could not cast. Metadata ID not valid", {icon:"bi-exclamation-triangle", iconColor:"#FF0000"});
                             }
                         }
                     }
