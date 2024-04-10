92ae748a763a74d9aae94f8503dd4c54fb0c0a17
Michael Yagi <michaeltyagi@gmail.com>
2024-04-10 07:23:13 -0700

    Tweaked UI

--- src/main/kotlin/com/miyagi/shashin/controller/SettingsController.kt
+++ src/main/kotlin/com/miyagi/shashin/controller/SettingsController.kt
@@ -486,10 +486,18 @@ class SettingsController {
             model["faceRecogAvailableStatusIcon"] = "bi-x-circle"
             model["faceRecogAvailableStatusColor"] = "red"
             model["faceRecogAvailableStatusText"] = "Could not connect to CompreFace server"
-            if (settings.getCompreFaceKey() == "") {
+            if (settings.getCompreFaceKey() == "" && settings.getCompreFaceServer() != "") {
                 model["faceRecogAvailableStatusIcon"] = "bi-exclamation-triangle"
                 model["faceRecogAvailableStatusColor"] = "orange"
                 model["faceRecogAvailableStatusText"] = "Enter a valid CompreFace key"
+            } else if (settings.getCompreFaceKey() != "" && settings.getCompreFaceServer() == "") {
+                model["faceRecogAvailableStatusIcon"] = "bi-exclamation-triangle"
+                model["faceRecogAvailableStatusColor"] = "orange"
+                model["faceRecogAvailableStatusText"] = "Enter a valid CompreFace server endpoint"
+            } else if (settings.getCompreFaceKey() == "" && settings.getCompreFaceServer() == "") {
+                model["faceRecogAvailableStatusIcon"] = "bi-info-circle"
+                model["faceRecogAvailableStatusColor"] = "gray"
+                model["faceRecogAvailableStatusText"] = "CompreFace not setup"
             } else if (faceRecogServicesAvailable) {
                 model["faceRecogAvailableStatusIcon"] = "bi-check-circle"
                 model["faceRecogAvailableStatusColor"] = "green"
