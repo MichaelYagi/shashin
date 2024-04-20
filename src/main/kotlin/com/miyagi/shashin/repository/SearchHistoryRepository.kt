0f950791918427a3ab386c23e63bb0f75bae5cb3
Michael Yagi <michaeltyagi@gmail.com>
2024-04-19 17:23:36 -0700

    Added search history for image scraper

--- src/main/kotlin/com/miyagi/shashin/repository/SearchHistoryRepository.kt
+++ src/main/kotlin/com/miyagi/shashin/repository/SearchHistoryRepository.kt
@@ -10,12 +10,21 @@ import org.springframework.stereotype.Repository
 interface SearchHistoryRepository : CrudRepository<SearchHistory?, Int?> {
     @Query("SELECT * FROM searchhistory WHERE user_id = :userId AND search_type = 1 ORDER BY created_at DESC LIMIT :limit", nativeQuery = true)
     fun findTopNByUserIdOrderByCreatedAtDesc(@Param("userId") userId: Int, @Param("limit") limit: Int): MutableIterable<SearchHistory>?
+    @Query("SELECT * FROM searchhistory WHERE user_id = :userId AND search_type = 2 ORDER BY created_at DESC LIMIT :limit", nativeQuery = true)
+    fun findImageScraperTopNByUserIdOrderByCreatedAtDesc(@Param("userId") userId: Int, @Param("limit") limit: Int): MutableIterable<SearchHistory>?
+    @Query("SELECT * FROM searchhistory WHERE user_id = :userId AND search_type = 2 ORDER BY id DESC LIMIT :limit", nativeQuery = true)
+    fun findImageScraperTopNByUserIdOrderByIdDesc(@Param("userId") userId: Int, @Param("limit") limit: Int): MutableIterable<SearchHistory>?
     @Query("SELECT * FROM searchhistory WHERE user_id = :userId AND search_type = 1 ORDER BY id DESC LIMIT :limit", nativeQuery = true)
     fun findTopNByUserIdOrderByIdDesc(@Param("userId") userId: Int, @Param("limit") limit: Int): MutableIterable<SearchHistory>?
     @Query("SELECT COUNT(*) FROM searchhistory WHERE user_id = :userId AND LOWER(term) = LOWER(:term) AND search_type = 1", nativeQuery = true)
     fun countByUserIdAndTermIgnoreCase(userId: Int, term: String): Int
     @Query("SELECT COUNT(*) FROM searchhistory WHERE user_id = :userId AND search_type = 1", nativeQuery = true)
     fun countByUserId(userId: Int): Int
+    @Query("SELECT COUNT(*) FROM searchhistory WHERE user_id = :userId AND search_type = 2", nativeQuery = true)
+    fun countImageScraperByUserId(userId: Int): Int
     @Query("SELECT * FROM searchhistory WHERE user_id = :userId AND LOWER(term) = LOWER(:term) AND search_type = 1 GROUP BY user_id, LOWER(term)", nativeQuery = true)
     fun findDistinctByUserIdAndTerm(@Param("userId") userId: Int, term: String): SearchHistory?
+    @Query("SELECT * FROM searchhistory WHERE user_id = :userId AND LOWER(term) = LOWER(:term) AND search_type = 2 GROUP BY user_id, LOWER(term)", nativeQuery = true)
+    fun findImageScraperDistinctByUserIdAndTerm(@Param("userId") userId: Int, term: String): SearchHistory?
+    fun deleteByIdAndSearchType(id: Int, type: Int): Long
 }
---
+++ src/main/kotlin/com/miyagi/shashin/util/SearchHistoryTypes.kt
@@ -0,0 +1,6 @@
+package com.miyagi.shashin.util
+
+enum class SearchHistoryTypes(val type: Int) {
+    AppHistorySearch(1),
+    UrlHistorySearch(2)
+}
