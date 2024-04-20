ee999a4e0dd6c7746d7abab7a642c6b8f175074b
Michael Yagi <michaeltyagi@gmail.com>
2024-04-19 18:49:52 -0700

    Added search history for image scraper

--- src/main/kotlin/com/miyagi/shashin/repository/SearchHistoryRepository.kt
+++ src/main/kotlin/com/miyagi/shashin/repository/SearchHistoryRepository.kt
@@ -8,25 +8,15 @@ import org.springframework.stereotype.Repository
 
 @Repository
 interface SearchHistoryRepository : CrudRepository<SearchHistory?, Int?> {
-    @Query("SELECT * FROM searchhistory WHERE user_id = :userId AND search_type = 1 ORDER BY created_at DESC LIMIT :limit", nativeQuery = true)
-    fun findTopNByUserIdOrderByCreatedAtDesc(@Param("userId") userId: Int, @Param("limit") limit: Int): MutableIterable<SearchHistory>?
-    @Query("SELECT * FROM searchhistory WHERE user_id = :userId AND search_type = 2 ORDER BY created_at DESC LIMIT :limit", nativeQuery = true)
-    fun findImageScraperTopNByUserIdOrderByCreatedAtDesc(@Param("userId") userId: Int, @Param("limit") limit: Int): MutableIterable<SearchHistory>?
-    @Query("SELECT * FROM searchhistory WHERE user_id = :userId AND search_type = 2 ORDER BY id DESC LIMIT :limit", nativeQuery = true)
-    fun findImageScraperTopNByUserIdOrderByIdDesc(@Param("userId") userId: Int, @Param("limit") limit: Int): MutableIterable<SearchHistory>?
-    @Query("SELECT * FROM searchhistory WHERE user_id = :userId AND search_type = 1 ORDER BY id DESC LIMIT :limit", nativeQuery = true)
-    fun findTopNByUserIdOrderByIdDesc(@Param("userId") userId: Int, @Param("limit") limit: Int): MutableIterable<SearchHistory>?
-    @Query("SELECT COUNT(*) FROM searchhistory WHERE user_id = :userId AND LOWER(term) = LOWER(:term) AND search_type = 1", nativeQuery = true)
-    fun countByUserIdAndTermIgnoreCase(userId: Int, term: String): Int
-    @Query("SELECT COUNT(*) FROM searchhistory WHERE user_id = :userId AND LOWER(term) = LOWER(:term) AND search_type = 2", nativeQuery = true)
-    fun countImageScraperByUserIdAndTermIgnoreCase(userId: Int, term: String): Int
-    @Query("SELECT COUNT(*) FROM searchhistory WHERE user_id = :userId AND search_type = 1", nativeQuery = true)
-    fun countByUserId(userId: Int): Int
-    @Query("SELECT COUNT(*) FROM searchhistory WHERE user_id = :userId AND search_type = 2", nativeQuery = true)
-    fun countImageScraperByUserId(userId: Int): Int
-    @Query("SELECT * FROM searchhistory WHERE user_id = :userId AND LOWER(term) = LOWER(:term) AND search_type = 1 GROUP BY user_id, LOWER(term)", nativeQuery = true)
-    fun findDistinctByUserIdAndTerm(@Param("userId") userId: Int, term: String): SearchHistory?
-    @Query("SELECT * FROM searchhistory WHERE user_id = :userId AND LOWER(term) = LOWER(:term) AND search_type = 2 GROUP BY user_id, LOWER(term)", nativeQuery = true)
-    fun findImageScraperDistinctByUserIdAndTerm(@Param("userId") userId: Int, term: String): SearchHistory?
+    @Query("SELECT * FROM searchhistory WHERE user_id = :userId AND search_type = :type ORDER BY created_at DESC LIMIT :limit", nativeQuery = true)
+    fun findTopNByUserIdOrderByCreatedAtDesc(@Param("userId") userId: Int, @Param("limit") limit: Int, type: Int): MutableIterable<SearchHistory>?
+    @Query("SELECT * FROM searchhistory WHERE user_id = :userId AND search_type = :type ORDER BY id DESC LIMIT :limit", nativeQuery = true)
+    fun findTopNByUserIdOrderByIdDesc(@Param("userId") userId: Int, @Param("limit") limit: Int, type: Int): MutableIterable<SearchHistory>?
+    @Query("SELECT COUNT(*) FROM searchhistory WHERE user_id = :userId AND LOWER(term) = LOWER(:term) AND search_type = :type", nativeQuery = true)
+    fun countByUserIdAndTermIgnoreCase(userId: Int, term: String, type: Int): Int
+    @Query("SELECT COUNT(*) FROM searchhistory WHERE user_id = :userId AND search_type = :type", nativeQuery = true)
+    fun countByUserId(userId: Int, type: Int): Int
+    @Query("SELECT * FROM searchhistory WHERE user_id = :userId AND LOWER(term) = LOWER(:term) AND search_type = :type GROUP BY user_id, LOWER(term)", nativeQuery = true)
+    fun findDistinctByUserIdAndTerm(@Param("userId") userId: Int, term: String, type: Int): SearchHistory?
     fun deleteByIdAndSearchType(id: Int, type: Int): Long
 }
