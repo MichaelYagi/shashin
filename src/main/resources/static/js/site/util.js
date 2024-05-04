e99d93651ac692677d767f8f581738d83a2535fc
Michael Yagi <michaeltyagi@gmail.com>
2024-05-03 21:06:52 -0700

    Added google lens link

--- src/main/resources/static/js/site/util.js
+++ src/main/resources/static/js/site/util.js
@@ -453,6 +453,15 @@ class Util {
         }
     }
 
+    static isLocalNetwork(hostname = window.location.hostname) {
+        return (
+            (['localhost', '127.0.0.1', '', '::1'].includes(hostname))
+            || (hostname.startsWith('192.168.'))
+            || (hostname.startsWith('10.'))
+            || (hostname.endsWith('.local'))
+        )
+    }
+
     static isMobile() {
         let isMobile = false; //initiate as false
         // device detection
@@ -1376,6 +1385,10 @@ class Util {
             let shareDetailsHtml = "<a class='bi-download' href='" + relativeShareLink + "/download' title='Download photo' style='font-size: 1rem;'></a>&nbsp;&nbsp;&nbsp;" +
                 // "<a href='#' class='copyLink bi-clipboard-plus sharecopy' data-clipboard-text='" + shareUrl + page + "' title='Copy share link' style='font-size: 1rem;' id='shareCopyLink'></a>&nbsp;&nbsp;&nbsp;" +
                 "<a class='"+((metadata.videoUrl != null) ? "bi-camera-video":"bi-file-image")+"' style='font-size: 1rem;' href='" + relativeShareLink.replace('/api/v1','') + page + "' title='View "+((metadata.videoUrl != null) ? "video":"image")+"' target='_blank'></a>";
+
+            if (metadata.videoUrl === null && Util.isLocalNetwork() === true) {
+                shareDetailsHtml += "&nbsp;&nbsp;&nbsp;<a class='bi-camera' style='font-size: 1rem;' href='https://lens.google.com/uploadbyurl?url="+relativeShareLink+"' title='Search Google Lens' target='_blank'></a>";
+            }
 
             if ($(".shareUrlDetailsA").length > 0) {
                 shareDetailsHtml += timeLinkHtml;
