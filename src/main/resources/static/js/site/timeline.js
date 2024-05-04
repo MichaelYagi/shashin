49777beceef2cad39af1f0a3aa502647f243acef
Michael Yagi <michaeltyagi@gmail.com>
2024-05-04 08:08:36 -0700

    Fixing timeline scrolling

--- src/main/resources/static/js/site/timeline.js
+++ src/main/resources/static/js/site/timeline.js
@@ -934,22 +934,21 @@
                         // Stage 2 - network call to create image placeholders and UI skeleton for month
                         const msg = await timelineSettings.updateTimeline(currentDate, mediaTypeFilter, action, anchorPoint);
 
+                        // Stage 3 - network call to embed the image URL and complete the process
                         if (timelineSettings.initialized === false) {
                             if (msg === timelineSettings.success && $("#" + currentDate).length === 1) {
                                 await timelineSettings.attachAssociatedMetadata(currentDate, mediaTypeFilter);
                                 timelineSettings.distanceToFooter = calculateDistanceToFooter();
                             }
+                        } else {
+                            // 1 sec delay for smoother scrolling
+                            setTimeout(async () => {
+                                if (msg === timelineSettings.success && $("#" + currentDate).length === 1) {
+                                    await timelineSettings.attachAssociatedMetadata(currentDate, mediaTypeFilter);
+                                    timelineSettings.distanceToFooter = calculateDistanceToFooter();
+                                }
+                            }, 1000);
                         }
-                        // else {
-                        //     // 1 sec delay for smoother scrolling
-                        //     setTimeout(async () => {
-                        //         // Stage 3 - network call to embed the image URL and complete the process
-                        //         if (msg === timelineSettings.success && $("#" + currentDate).length === 1) {
-                        //             await timelineSettings.attachAssociatedMetadata(currentDate, mediaTypeFilter);
-                        //             timelineSettings.distanceToFooter = calculateDistanceToFooter();
-                        //         }
-                        //     }, 1000);
-                        // }
 
                         timelineSettings.distanceToFooter = calculateDistanceToFooter();
 
