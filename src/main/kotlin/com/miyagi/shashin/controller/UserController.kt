e4bb2a0e508ad1cb9676d86c3d4287bbf890e165
Michael Yagi <michaeltyagi@gmail.com>
2024-05-09 07:07:02 -0700

    Fixing dates

--- src/main/kotlin/com/miyagi/shashin/controller/UserController.kt
+++ src/main/kotlin/com/miyagi/shashin/controller/UserController.kt
@@ -157,6 +157,14 @@ class UserController {
         val currentUserObj = model.getAttribute("currentUser") as User?
         if (currentUserObj != null) {
             model["user"] = currentUserObj
+
+            if (currentUserObj.getAuthority() == "ROLE_SUPER") {
+                model["message"] = "<span>You have <strong>Super Admin</strong> privileges.</span>"
+            } else if (currentUserObj.getAuthority() == "ROLE_ADMIN") {
+                model["message"] = "<span>You have <strong>Admin</strong> privileges.</span>"
+            } else {
+                model["message"] = "<span>You have <strong>User</strong> privileges.</span>"
+            }
         }
 
         val module = "profile"
@@ -285,14 +293,6 @@ class UserController {
         val currentUserObj = model.getAttribute("currentUser") as User?
         if (currentUserObj != null) {
             model["user"] = currentUserObj
-
-            if (currentUserObj.getAuthority() == "ROLE_SUPER") {
-                model["message"] = "<span>You have <strong>Super Admin</strong> privileges.</span>"
-            } else if (currentUserObj.getAuthority() == "ROLE_ADMIN") {
-                model["message"] = "<span>You have <strong>Admin</strong> privileges.</span>"
-            } else {
-                model["message"] = "<span>You have <strong>User</strong> privileges.</span>"
-            }
 
             var baseUrlBuilder = ServletUriComponentsBuilder.fromRequestUri(request).replacePath(null)
             if (request.scheme == "https") {
