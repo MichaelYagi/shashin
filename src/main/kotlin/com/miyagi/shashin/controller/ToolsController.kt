c84f78de52f6c8d31a32f61cab055c6b39c2105b
Michael Yagi <michaeltyagi@gmail.com>
2024-05-18 22:17:58 -0700

    Updated health page and API

--- src/main/kotlin/com/miyagi/shashin/controller/ToolsController.kt
+++ src/main/kotlin/com/miyagi/shashin/controller/ToolsController.kt
@@ -613,7 +613,7 @@ class ToolsController {
         var status = "OK"
 
         response["status"] = status
-        response["healthEndpointRequestTiming"] = 0
+        response["healthEndpointTiming"] = 0
 
         val runtimeMXBean: RuntimeMXBean = ManagementFactory.getRuntimeMXBean()
         val seconds: Long = runtimeMXBean.uptime / 1000
@@ -699,7 +699,7 @@ class ToolsController {
 
         val requestTimingDiff: Long = requestTimingEnd.time - requestTimingStart.time
 
-        response["healthEndpointRequestTiming"] = SimpleDateFormat("mm:ss:SSS").format(Date(requestTimingDiff))
+        response["healthEndpointTiming"] = SimpleDateFormat("mm:ss:SSS").format(Date(requestTimingDiff))
 
         response["status"] = status
 
