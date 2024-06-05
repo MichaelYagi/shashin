0dabc4955379811572040aee140688d7bac0a98d
Michael Yagi <michaeltyagi@gmail.com>
2024-06-04 17:34:05 -0700

    Increased element scan timeout value

--- src/test/com/miyagi/shashin/unit/FileUtilsTest.kt
+++ src/test/com/miyagi/shashin/unit/FileUtilsTest.kt
@@ -18,4 +18,32 @@ class FileUtilsTest {
         byteArray = FileUtils.parseBase64("")
         Assertions.assertNull(byteArray)
     }
+
+    @Test
+    fun convertYamlToJsonTest() {
+        var json = FileUtils.convertYamlToJson("french-hens: 3")
+        Assertions.assertEquals(json, "{\"french-hens\":3}")
+
+        json = FileUtils.convertYamlToJson(
+                " calling-birds:\n   - huey\n   - dewey")
+        Assertions.assertEquals(json, "{\"calling-birds\":[\"huey\",\"dewey\"]}")
+
+        json = FileUtils.convertYamlToJson("ray: \"a drop of golden sun\"\npi: 3.14159")
+        Assertions.assertEquals(json, "{\"ray\":\"a drop of golden sun\",\"pi\":3.14159}")
+
+        json = FileUtils.convertYamlToJson("french-hens: 3\ncalling-birds:\n   - huey\n   - dewey")
+        Assertions.assertEquals(json, "{\"french-hens\":3,\"calling-birds\":[\"huey\",\"dewey\"]}")
+
+        json = FileUtils.convertYamlToJson("xmas-fifth-day:\n   golden-rings: 5\n   partridges:\n     count: 1")
+        Assertions.assertEquals(json, "{\"xmas-fifth-day\":{\"golden-rings\":5,\"partridges\":{\"count\":1}}}")
+
+        json = FileUtils.convertYamlToJson("french-hens:3")
+        Assertions.assertEquals(json, "")
+
+        json = FileUtils.convertYamlToJson(" calling-birds:\n  - huey\n   - dewey")
+        Assertions.assertEquals(json, "{\"calling-birds\":[\"huey - dewey\"]}")
+
+        json = FileUtils.convertYamlToJson("---\nQuickTimeSound-SampleRate: 48000")
+        Assertions.assertEquals(json, "{\"QuickTimeSound-SampleRate\":48000}")
+    }
 }
