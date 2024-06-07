af2966d729dea3d6992fad684e5b71bd59b9a53e
Michael Yagi <michaeltyagi@gmail.com>
2024-06-06 21:31:55 -0700

    Added API tests

--- src/test/com/miyagi/shashin/e2e/APITests.kt
+++ src/test/com/miyagi/shashin/e2e/APITests.kt
@@ -2,16 +2,13 @@ package com.miyagi.shashin.e2e
 
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.miyagi.shashin.controller.ToolsController
 import com.miyagi.shashin.model.User
 import com.miyagi.shashin.repository.UserRepository
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.openqa.selenium.By
-import org.openqa.selenium.JavascriptExecutor
 import org.openqa.selenium.WebElement
-import org.openqa.selenium.interactions.Actions
 import org.openqa.selenium.support.ui.ExpectedConditions
 import org.openqa.selenium.support.ui.WebDriverWait
 import org.springframework.beans.factory.annotation.Autowired
@@ -26,25 +23,18 @@ import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import org.springframework.web.context.WebApplicationContext
-import org.springframework.web.reactive.function.BodyInserters
 import org.springframework.web.reactive.function.client.WebClient
 import java.io.File
 import java.net.URL
-import java.util.concurrent.TimeUnit
 import java.util.logging.Level
-import java.util.regex.Pattern
-
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 class APITests: BaseSeleniumTests() {
 
     @Autowired
-    private lateinit var toolsController: ToolsController
-    private var metadataId: String? = null
     private var adminId: Int? = null
     private var userId: Int? = null
-    private var albumId: Int? = null
     private var mockMvc: MockMvc? = null
 
     @LocalServerPort
@@ -79,6 +69,53 @@ class APITests: BaseSeleniumTests() {
         userObj.setApikey("00000000-00000000-00000000-00000001")
         userRepository?.save(userObj)
         userId = userObj.getId()
+
+        mockMvc = MockMvcBuilders
+            .webAppContextSetup(context!!)
+            .apply<DefaultMockMvcBuilder>(SecurityMockMvcConfigurers.springSecurity())
+            .build()
+
+        this.driver?.get("http://localhost:$port/users/login")
+        //println(this.driver?.pageSource)
+        val username = this.driver!!.findElement(By.id("username"))
+        val password = this.driver!!.findElement(By.id("password"))
+        val rememberMe = this.driver!!.findElement(By.id("remember-me"))
+        val login = this.driver!!.findElement(By.id("submit-loginreg"))
+        rememberMe.click()
+        username.sendKeys("testadmin")
+        password.sendKeys("testadmin")
+        login.click()
+
+        this.driver!!.get("http://localhost:$port/settings")
+
+        // Get test image data and populate in settings
+        val classLoader = javaClass.classLoader
+        val testImageUrl: URL = classLoader.getResource("testscreen.jpg")!!
+        val testImageFile = File(testImageUrl.file)
+        val mediaDirTextArea = this.driver!!.findElement(By.id("mediaDirTextArea"))
+        mediaDirTextArea.sendKeys(testImageFile.parent)
+        val scanAutomatically = this.driver!!.findElement(By.id("scanAutomatically"))
+        if (scanAutomatically.isSelected) {
+            scanAutomatically.click()
+        }
+
+        val saveSettings = this.driver!!.findElement(By.id("saveSettings"))
+        saveSettings.click()
+        WebDriverWait(this.driver, 30).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[contains(text(),'Settings saved')]")))
+
+        // Scan new image
+        this.driver!!.get("http://localhost:$port/settings/scan")
+        val scanBeforeBody = this.driver!!.findElement(By.tagName("body"))
+        val scanPhotos = this.driver!!.findElement(By.id("scanPhotos"))
+        scanPhotos.click()
+
+        // Indicates scanning something
+        var scanBeforeAfter: WebElement? = null
+        var startTime = System.currentTimeMillis()
+        while (scanBeforeBody != scanBeforeAfter || (System.currentTimeMillis()-startTime)<this.elementScanTimeoutMillis) {
+            scanBeforeAfter = this.driver!!.findElement(By.tagName("body"))
+        }
+        this.logger.log(Level.INFO, "APITests - Photos scanned.")
     }
 
     @Test
@@ -133,5 +170,51 @@ class APITests: BaseSeleniumTests() {
         }
 
         Assertions.assertTrue(singleStatus == "OK")
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun browseControllerAPITests() {
+        val webClient = WebClient.create("http://localhost:$port/")
+
+        var jsonString: String? = null
+        var jsonNode: JsonNode? = null
+        val mapper = ObjectMapper()
+
+        try {
+            val response = webClient.get()
+                .uri("/api/v1/recent")
+                .header("x-api-key", "00000000-00000000-00000000-00000000")
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON.toString())
+                .retrieve()
+                .bodyToMono(String::class.java)
+                .block()
+
+            jsonString = response
+        } catch (_: Exception) {}
+
+        if (!jsonString.isNullOrBlank()) {
+            jsonNode = mapper.readTree(jsonString)
+        }
+
+        Assertions.assertTrue(jsonNode!!.has("metadataList"))
+
+        var result: String? = ""
+        try {
+            val response = webClient.get()
+                .uri("api/v1/recent")
+                .header("x-api-key", "00000000-00000000-00000000-00000001")
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON.toString())
+                .retrieve()
+                .bodyToMono(String::class.java)
+                .block()
+
+            result = response
+        } catch (e: Exception) {
+            result = e.message
+        }
+
+        // 403 forbidden
+        Assertions.assertTrue(result!!.contains("403"))
     }
 }
