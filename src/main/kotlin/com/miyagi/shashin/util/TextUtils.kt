cc13394bb412df27d8c40e9ae66d9d156c4d5750
Michael Yagi <michaeltyagi@gmail.com>
2024-08-01 07:47:11 -0700

    Updated driver selection in tests

--- src/main/kotlin/com/miyagi/shashin/util/TextUtils.kt
+++ src/main/kotlin/com/miyagi/shashin/util/TextUtils.kt
@@ -333,21 +333,15 @@ class TextUtils {
                     }
 
                     if (addressObj.has("address")) {
-                        var addressFound = false
-
                         if (addressObj.get("address").has("house_number") && addressObj.get("address").get("house_number") != null && addressObj.get("address").get("house_number").textValue() != "") {
                             buildPlace += addressObj.get("address").get("house_number").textValue() + " "
                             logger.log(Level.INFO, "house_number found: ${addressObj.get("address").get("house_number").textValue()}")
-                            addressFound = true
                         }
 
                         if (addressObj.get("address").has("road") && addressObj.get("address").get("road") != null && addressObj.get("address").get("road").textValue() != "") {
                             buildPlace += addressObj.get("address").get("road").textValue() + ", "
                             logger.log(Level.INFO, "road found: ${addressObj.get("address").get("road").textValue()}")
-                            addressFound = true
-                        }
-
-                        if (!addressFound && addressObj.get("address").has("neighbourhood") && addressObj.get("address").get("neighbourhood") != null && addressObj.get("address").get("neighbourhood").textValue() != "") {
+                        } else if (addressObj.get("address").has("neighbourhood") && addressObj.get("address").get("neighbourhood") != null && addressObj.get("address").get("neighbourhood").textValue() != "") {
                             buildPlace += addressObj.get("address").get("neighbourhood").textValue() + ", "
                             logger.log(Level.INFO, "neighbourhood found: ${addressObj.get("address").get("neighbourhood").textValue()}")
                         }
