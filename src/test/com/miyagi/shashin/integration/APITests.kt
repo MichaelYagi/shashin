b68cd0bb7defd7610b1f9f6fe138a0541928f176
Michael Yagi <michaeltyagi@gmail.com>
2024-09-07 19:22:39 -0700

    [skip ci] Adding comments tests

--- src/test/com/miyagi/shashin/integration/APITests.kt
+++ src/test/com/miyagi/shashin/integration/APITests.kt
@@ -12,7 +12,6 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity
