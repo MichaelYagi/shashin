fe5a7f6879b6a5f4f19c25a5956afad31edf7766
Michael Yagi <michaeltyagi@gmail.com>
2024-09-11 18:04:18 -0700

    [skip ci] Timeline scrolling fixes

--- src/main/resources/static/js/site/timeline.js
+++ src/main/resources/static/js/site/timeline.js
@@ -136,7 +136,10 @@
                     if ($(".attachMetadataPhotos").last().text() !== "EOL" && $("#spinner_bottom").css("display") === "block") {
                         timelineSettings.enableScrollSpy = true;
                         renderViewport();
-                        scrollByN(-1);
+
+                        if ($(".attachMetadataPhotos").last().text() !== "EOL") {
+                            scrollByN(-1);
+                        }
                     }
                 }, 1000);
             }
