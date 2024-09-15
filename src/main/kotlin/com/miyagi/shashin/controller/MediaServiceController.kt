9fe1022bc1b4292247a67826fb6b1ce551185ce5
Michael Yagi <michaeltyagi@gmail.com>
2024-09-14 20:25:27 -0700

    [skip ci] Added full slideshow

--- src/main/kotlin/com/miyagi/shashin/controller/MediaServiceController.kt
+++ src/main/kotlin/com/miyagi/shashin/controller/MediaServiceController.kt
@@ -397,6 +397,13 @@ class MediaServiceController {
 
             if (randomMetadata != null) {
                 resp["metadata"] = randomMetadata
+                val metadataList = mutableListOf<Metadata>()
+                metadataList.add(randomMetadata)
+                val placeNamesList = TextUtils.sortPlaceNames(metadataList)
+                resp["shortPlaceName"] = ""
+                if (placeNamesList.isNotEmpty()) {
+                    resp["shortPlaceName"] = placeNamesList[0]
+                }
                 resp["status"] = ApiResponse.SUCCESS.status
                 resp["msg"] = ""
                 logger.log(Level.INFO, "Random image metadata ID ${randomMetadata.getId()}")
@@ -434,6 +441,13 @@ class MediaServiceController {
 
             if (randomMetadata != null) {
                 resp["metadata"] = randomMetadata
+                val metadataList = mutableListOf<Metadata>()
+                metadataList.add(randomMetadata)
+                val placeNamesList = TextUtils.sortPlaceNames(metadataList)
+                resp["shortPlaceName"] = ""
+                if (placeNamesList.isNotEmpty()) {
+                    resp["shortPlaceName"] = placeNamesList[0]
+                }
                 resp["status"] = ApiResponse.SUCCESS.status
                 resp["msg"] = ""
                 logger.log(Level.INFO, "Random image metadata ID ${randomMetadata.getId()}")
