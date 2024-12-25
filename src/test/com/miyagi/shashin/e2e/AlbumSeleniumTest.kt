06b72232e1ad29d34f3e8c9f1113cded8671354b
Michael Yagi <michaeltyagi@gmail.com>
2024-12-25 17:03:53 +0900

    Updating tests

--- src/test/com/miyagi/shashin/e2e/AlbumSeleniumTest.kt
+++ src/test/com/miyagi/shashin/e2e/AlbumSeleniumTest.kt
@@ -305,7 +305,7 @@ class AlbumSeleniumTest: BaseSeleniumTests() {
     fun shouldViewInAlbumAndCommentAsUser() {
         //Login as testuser
         this.driver!!.get("http://localhost:$port/users/logout")
-//        this.driver!!.get("http://localhost:$port/users/logout")
+        this.driver!!.get("http://localhost:$port/users/logout")
 //        this.driver?.get("http://localhost:$port/users/login")
 //        WebDriverWait(this.driver, Duration.ofSeconds(this.elementWaitSeconds)).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[contains(text(),'Please Login')]")))
         val username = this.driver!!.findElement(By.id("username"))
