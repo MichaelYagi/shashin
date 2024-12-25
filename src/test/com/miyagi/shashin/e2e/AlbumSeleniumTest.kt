2813f7fa87de622554051044864f689328730203
Michael Yagi <michaeltyagi@gmail.com>
2024-12-25 17:18:46 +0900

    Updating tests

--- src/test/com/miyagi/shashin/e2e/AlbumSeleniumTest.kt
+++ src/test/com/miyagi/shashin/e2e/AlbumSeleniumTest.kt
@@ -305,9 +305,10 @@ class AlbumSeleniumTest: BaseSeleniumTests() {
     fun shouldViewInAlbumAndCommentAsUser() {
         //Login as testuser
         this.driver!!.get("http://localhost:$port/users/logout")
-        this.driver!!.get("http://localhost:$port/users/logout")
+        println(this.driver?.pageSource)
+//        this.driver!!.get("http://localhost:$port/users/logout")
 //        this.driver?.get("http://localhost:$port/users/login")
-//        WebDriverWait(this.driver, Duration.ofSeconds(this.elementWaitSeconds)).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[contains(text(),'Please Login')]")))
+        WebDriverWait(this.driver, Duration.ofSeconds(this.elementWaitSeconds)).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[contains(text(),'Please Login')]")))
         val username = this.driver!!.findElement(By.id("username"))
         val password = this.driver!!.findElement(By.id("password"))
         val rememberMe = this.driver!!.findElement(By.id("remember-me"))
