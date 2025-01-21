64972bb7fc83b2c0f0ef0541c7b1a4e2556cf72b
Michael Yagi <michaeltyagi@gmail.com>
2025-01-20 19:47:52 -0800

    Bug fix in return content type

--- src/main/resources/static/js/site/app.js
+++ src/main/resources/static/js/site/app.js
@@ -957,18 +957,6 @@
                         $(element).children('img').css("z-index", 0);
                     });
                 }
-
-                // Replace with small dot image or blur image
-                // const elementsImagesNotInViewport = Util.elementsNotInViewport($('img[src*="_225.jpg"].photo-thumbnail-image'));
-                // $(elementsImagesNotInViewport).attr("src", "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=");
-                // $(elementsImagesNotInViewport).css("z-index", -1);
-                //
-                // const elementsNotInViewport = Util.elementsNotInViewport($('img[src*="data:image/gif"].photo-thumbnail-image'));
-                // $.map(elementsNotInViewport, function (element) {
-                //     if ($(element).attr("data-xsmallthumb") !== "") {
-                //         $(element).attr("src", $(element).attr("data-xsmallthumb"));
-                //     }
-                // });
             }
         }
 
