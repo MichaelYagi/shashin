424b528e3b4d15cc7a882947d275e5d021582553
Michael Yagi <michaeltyagi@gmail.com>
2025-02-03 17:26:41 -0800

    [skip ci] Thumbnails not draggable

--- src/main/resources/static/js/site/album.js
+++ src/main/resources/static/js/site/album.js
@@ -75,7 +75,7 @@
                 $("#metadataId").val(metadata.id);
                 if (metadata.thumbnailUrlCentered !== null) {
                     const version = Util.getMetadataLocalStorage();
-                    $("#propAlbumModalThumbnail").html('<img loading="lazy" src="/api/v1/thumbnails/centered/' + metadata.id + (version === "" ? "" : "?v=" + version) + '" height="100" width="100">');
+                    $("#propAlbumModalThumbnail").html('<img loading="lazy" src="/api/v1/thumbnails/centered/' + metadata.id + (version === "" ? "" : "?v=" + version) + '" height="100" width="100" draggable="false">');
                 }
 
                 // Open modal window
