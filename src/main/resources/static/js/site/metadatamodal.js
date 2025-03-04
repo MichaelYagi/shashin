4c0d1df90af51abb1b2b13b47553ccac90ea6834
Michael Yagi <michaeltyagi@gmail.com>
2025-03-04 07:44:30 -0800

    [skip ci] Truncate location on desktop

--- src/main/resources/static/js/site/metadatamodal.js
+++ src/main/resources/static/js/site/metadatamodal.js
@@ -350,7 +350,11 @@ async function saveMetadata(e) {
                         // }
 
                         if (data.hasOwnProperty("shortPlaceName") && data.shortPlaceName !== "") {
-                            $("#shortLocationLabel").html(data.shortPlaceName);
+                            let locationLabel = data.shortPlaceName;
+                            if (Util.isMobile() === false) {
+                                locationLabel = $.trim(data.shortPlaceName).substring(0, 39).trim(this) + "...";
+                            }
+                            $("#shortLocationLabel").html(locationLabel);
                         }
 
                     }
