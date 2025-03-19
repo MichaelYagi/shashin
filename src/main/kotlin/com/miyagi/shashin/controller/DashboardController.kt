2ce40891346b21a48bcf13436f03295e27b223d5
Michael Yagi <michaeltyagi@gmail.com>
2025-03-19 06:38:31 -0700

    [skip ci] Updated js

--- src/main/kotlin/com/miyagi/shashin/controller/DashboardController.kt
+++ src/main/kotlin/com/miyagi/shashin/controller/DashboardController.kt
@@ -101,9 +101,11 @@ class DashboardController {
 
         val osMXBean: OperatingSystemMXBean = ManagementFactory.getOperatingSystemMXBean() as OperatingSystemMXBean
 
+        val processCpuLoad = osMXBean.processCpuLoad
+        logger.log(Level.INFO, "Process CPU load:"+(processCpuLoad * 100).toInt())
 //        println("Process CPU load:"+(osMXBean.processCpuLoad * 100).toInt())
 //        println("System CPU load:"+(osMXBean.cpuLoad * 100).toInt())
-        metricsMap["processCpuLoadPercentDouble"] = osMXBean.processCpuLoad
+        metricsMap["processCpuLoadPercentDouble"] = processCpuLoad
         @Suppress("DEPRECATION")
         metricsMap["systemCpuLoadPercentDouble"] = osMXBean.systemCpuLoad
         val dtf = DateTimeFormatter.ofPattern("HH:mm:ss")
