471b21e4e98527d5fac52edd11ecc1825f7bc5c8
Michael Yagi <michaeltyagi@gmail.com>
2025-03-22 08:05:53 -0700

    [skip ci] Updated GH key

--- src/main/kotlin/com/miyagi/shashin/util/TextUtils.kt
+++ src/main/kotlin/com/miyagi/shashin/util/TextUtils.kt
@@ -1303,7 +1303,7 @@ class TextUtils {
 
         // https://docs.github.com/en/rest/repos/repos?apiVersion=2022-11-28#list-repository-tags
         fun getReleases(githubKey: String): MutableList<Map<String, Any>>? {
-            var url = "https://api.github.com/repos/michaelyagi/shashin/tags"
+            var url = "https://api.github.com/repos/michaelyagi/shashin/releases"
             var array: MutableList<Map<String, Any>>? = mutableListOf<Map<String, Any>>()
 
             try {
