f3e1071705dfb2fdc6cef6d38a435bae592ffed2
Michael Yagi <michaeltyagi@gmail.com>
2025-03-23 10:28:36 -0700

    [skip ci] Detailed login information

--- src/main/kotlin/com/miyagi/shashin/component/AuthFailureHandler.kt
+++ src/main/kotlin/com/miyagi/shashin/component/AuthFailureHandler.kt
@@ -81,7 +81,7 @@ class AuthFailureHandler : SimpleUrlAuthenticationFailureHandler() {
 
         val clientIP = TextUtils.getClientIp(request)
 
-        var ipString = ""
+        var ipString = "$clientIP "
         if (!TextUtils.isLocalIp(clientIP)) {
             ipString = "from IP <a href='https://ipgeolocation.io/ip-location/$clientIP' target='_blank'>$clientIP</a> "
         }
