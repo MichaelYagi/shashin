77bd6334031fd4e07232dceee40d3e47e45c971c
Michael Yagi <michaeltyagi@gmail.com>
2025-03-27 07:54:10 -0700

    [skip ci] Updated safari endpoints

--- src/main/kotlin/com/miyagi/shashin/controller/UserController.kt
+++ src/main/kotlin/com/miyagi/shashin/controller/UserController.kt
@@ -590,7 +590,7 @@ class UserController {
         } else if ((model.getAttribute("authority").toString() == model.getAttribute("adminRole") || model.getAttribute("authority").toString() == model.getAttribute("superRole")) && model.getAttribute("agentName") != "safari") {
             return "redirect:/timeline"
         } else if ((model.getAttribute("authority").toString() == model.getAttribute("adminRole") || model.getAttribute("authority").toString() == model.getAttribute("superRole")) && model.getAttribute("agentName") == "safari") {
-            return "redirect:/recent"
+            return "redirect:/taken"
         } else if (model.getAttribute("authority").toString() == model.getAttribute("userRole")) {
             return "redirect:/albums"
         } else {
