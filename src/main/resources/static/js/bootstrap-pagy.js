875c046054ba37735412cf0d866914708bdf6598
Michael Yagi <michaeltyagi@gmail.com>
2025-04-05 21:51:52 -0700

    [skip ci] Changing pagination library

--- src/main/resources/static/js/bootstrap-pagy.js
+++ src/main/resources/static/js/bootstrap-pagy.js
@@ -15,7 +15,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * ======================================================================== */
+ * ========================================================================
+ * MY: Modified to adapt to Shashin - 2025-04-05 */
 
 
 +function ($) { "use strict";
