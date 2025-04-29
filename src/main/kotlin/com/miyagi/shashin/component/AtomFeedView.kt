182e990827ee6e22f7eb4792b8e0fe432392f52c
Michael Yagi <michaeltyagi@gmail.com>
2025-04-29 11:52:24 -0700

    [skip ci] Adding slideshow filter

--- src/main/kotlin/com/miyagi/shashin/component/AtomFeedView.kt
+++ src/main/kotlin/com/miyagi/shashin/component/AtomFeedView.kt
@@ -145,15 +145,14 @@ class AtomFeedView : AbstractAtomFeedView() {
 
                     var place = ""
                     var metadataDescription = ""
-                    var albumName = ""
                     if (metadata.getPlaceName() != null && metadata.getPlaceName() != "") {
                         val placeArray = metadata.getPlaceName()!!.split(";")
-                        place = placeArray[0].trim() + " - "
+                        place = placeArray[0].trim()
                     }
                     if (metadata.getDescription() != null && metadata.getDescription() != "") {
-                        metadataDescription = metadata.getDescription()!!.trim() + " - "
+                        metadataDescription = metadata.getDescription()!!.trim()
                     }
-                    val descVal = "$albumName$metadataDescription$place"
+                    val descVal = "$metadataDescription<br>$place"
 
                     val content = Content()
                     content.type = "text/html"
