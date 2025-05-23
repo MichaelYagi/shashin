7c8d534f3ecf660de6a5f4efaf9c9df6a37a550b
Michael Yagi <michaeltyagi@gmail.com>
2025-05-23 15:07:39 -0700

    [skip ci] Updating test page

--- src/main/kotlin/com/miyagi/shashin/controller/TestController.kt
+++ src/main/kotlin/com/miyagi/shashin/controller/TestController.kt
@@ -121,7 +121,7 @@ class TestController {
 //            i.setGreyScale(true)
             val isDuplicate = i.isDuplicate()
 
-            response["text"] = "Is duplicate: " + isDuplicate + "<br>" + "Similarity: " + i.getSimilarity().toString()
+            response["text"] = "Is duplicate: " + isDuplicate + "<br>" + "Similarity: " + (100.0*i.getSimilarity()!!).toInt() + "%"
 
             // data:image/png;base64,
             val pre = "data:image/png;base64, "
