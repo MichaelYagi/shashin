c5a7682b81aea95789bc0df2e13542a3aef1768d
Michael Yagi <michaeltyagi@gmail.com>
2025-05-22 21:38:04 -0700

    [skip ci] Updating test page

--- src/main/resources/static/js/site/metadatabatchmodal.js
+++ src/main/resources/static/js/site/metadatabatchmodal.js
@@ -201,7 +201,7 @@ async function saveBatchMetadata(e) {
             setTimeout(function () {
                 const http = new Http("timeline");
                 http.ajax("get", "/timeline/yearmonthcounts/all").then(function (data) {
-                    if (data.hasOwnProperty("metadataDates") && data.hasOwnProperty("metadataYearMonthCount")) {
+                    if (data.hasOwnProperty("metadataDates") && data.hasOwnProperty("metadataYearMonthCount") && data.hasOwnProperty("metadataDatesHash")) {
                         timelineSettings.timelineDates = data.metadataDates;
                         timelineSettings.metadataYearMonthCount = data.metadataYearMonthCount;
                         timelineSettings.timelineDatesHash = data.metadataDatesHash;
