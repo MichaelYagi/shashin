c4ad61499f63cfe8b300051a78d322c672b4aa18
Michael Yagi <michaeltyagi@gmail.com>
2025-07-11 20:00:53 -0700

    [skip ci] Widened date select area

--- src/main/resources/static/js/site/album.js
+++ src/main/resources/static/js/site/album.js
@@ -190,7 +190,7 @@
                                 const uuid = uuidv4();
 
                                 if ($("#"+currentDate).length === 0 && dateHeadingObj !== null) {
-                                    const headerAndBody = '<section class="dateSection" id="'+dateHeadingObj.heading+'"><div class="mb-3 dateHeader" id="dateHeader'+dateHeadingObj.heading+'"><span id="select'+metadata.year+'-'+metadata.month+'-'+metadata.day+'" class="bi-circle pe-2 day-select" style="font-size: 0.85rem;color: lightgray;display: none"></span><strong>'+dateHeadingObj.display+'</strong></div><div id="dateBody'+dateHeadingObj.heading+'" class="row" class="row" style="margin-left:-2px;"></div></section>';
+                                    const headerAndBody = '<section class="dateSection" id="'+dateHeadingObj.heading+'"><div class="dateHeader" style="margin-left: -16px; padding-left: 13px;margin-top: -25px; padding-top: 25px;margin-bottom: 0; padding-bottom: 15px;" id="dateHeader'+dateHeadingObj.heading+'"><span id="select'+metadata.year+'-'+metadata.month+'-'+metadata.day+'" class="bi-circle pe-2 day-select" style="font-size: 0.85rem;color: lightgray;display: none"></span><strong>'+dateHeadingObj.display+'</strong></div><div id="dateBody'+dateHeadingObj.heading+'" class="row" class="row" style="margin-left:-2px;"></div></section>';
                                     $(headerAndBody).insertBefore($("." + appendClass).last());
                                 }
 
