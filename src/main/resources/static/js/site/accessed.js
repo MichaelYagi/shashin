f1db4dda0a6f41509f0ed22307a6f4da62ae4627
Michael Yagi <michaeltyagi@gmail.com>
2025-07-14 15:14:56 -0700

    [skip ci] Fixing UI

--- src/main/resources/static/js/site/accessed.js
+++ src/main/resources/static/js/site/accessed.js
@@ -113,7 +113,7 @@ class Accessed {
                         const uuid = uuidv4();
 
                         if ($("#"+currentDate).length === 0 && dateHeadingObj !== null) {
-                            const headerAndBody = '<section class="dateSection" id="'+currentDate+'"><p></p><div class="dateHeader" style="margin-left: -16px; padding-left: 13px;margin-top: -40px; padding-top: 25px;margin-bottom: -16px; padding-bottom: 15px;" id="dateHeader'+currentDate+'"><span id="select'+currentDate+'" class="bi-circle pe-2 day-select" style="font-size: 0.85rem;color: lightgray;display: none"></span><span class="text-muted">Accessed </span><strong>'+dateHeadingObj.display+'</strong>&nbsp;'+(dateHeadingObj.hasOwnProperty("placename")?dateHeadingObj.placename:'')+'</div><p></p><div id="dateBody'+currentDate+'" class="row" class="row" style="margin-left:-2px;"></div></section>';
+                            const headerAndBody = '<section class="dateSection" id="'+currentDate+'"><div class="dateHeader" style="margin-left: -16px; padding-left: 13px;margin-top: -24px; padding-top: 25px;margin-bottom: 0; padding-bottom: 15px;" id="dateHeader'+currentDate+'"><span id="select'+currentDate+'" class="bi-circle pe-2 day-select" style="font-size: 0.85rem;color: lightgray;display: none"></span><span class="text-muted">Accessed </span><strong>'+dateHeadingObj.display+'</strong>&nbsp;'+(dateHeadingObj.hasOwnProperty("placename")?dateHeadingObj.placename:'')+'</div><div id="dateBody'+currentDate+'" class="row" class="row" style="margin-left:-2px;"></div></section>';
                             $(headerAndBody).insertBefore($("." + appendClass).last());
                         }
 
