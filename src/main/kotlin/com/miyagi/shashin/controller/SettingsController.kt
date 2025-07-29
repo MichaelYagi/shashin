87850aba4e500de3b1c63777d57a72b3da20d4d7
Michael Yagi <michaeltyagi@gmail.com>
2025-07-29 07:36:19 -0700

    [skip ci] More translation tweaks using variables

--- src/main/kotlin/com/miyagi/shashin/controller/SettingsController.kt
+++ src/main/kotlin/com/miyagi/shashin/controller/SettingsController.kt
@@ -1956,7 +1956,7 @@ class SettingsController {
 
                     // Set notification for scanCount and date and link to /recent
                     var msg =
-                        messageSource?.getMessage("main.notification.setting.scan.pre", null, locale)+"<a href='/recent' target='_blank'>$metadataArrayCount&nbsp;"+messageSource?.getMessage("main.notification.setting.scan.post", null, locale)+"</a>"
+                        messageSource?.getMessage("main.notification.setting.scan", arrayOf("<a href='/recent' target='_blank'>$metadataArrayCount</a>"), locale)
                     if (recognitionCount > 0) {
                         msg += messageSource?.getMessage("main.notification.setting.scan.add.pre", null, locale)+"<a href='/people' target='_blank'>$recognitionCount"+messageSource?.getMessage("main.notification.setting.scan.add.post", null, locale)+"</a>"
                     }
