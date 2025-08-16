ec5b4f9b700f727f7f53ed63c17f4bd76f301109
Michael Yagi <michaeltyagi@gmail.com>
2025-08-16 07:33:39 -0700

    [skip ci] Updated version

--- src/main/resources/static/js/site/fragments/timeline/init.js
+++ src/main/resources/static/js/site/fragments/timeline/init.js
@@ -1,7 +1,7 @@
 (function( timelineSettings, $, undefined ) {
     // left: 37, up: 38, right: 39, down: 40,
-// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
-    var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+    // spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+    const keys = {37: 1, 38: 1, 39: 1, 40: 1};
 
     function preventDefault(e) {
         e.preventDefault();
