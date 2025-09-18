68f5012869c9134d15db15abbe98f60fc07b271d
Michael Yagi <michaeltyagi@gmail.com>
2025-09-18 13:15:40 -0700

    Attemtpting to fix tests

--- src/test/com/miyagi/shashin/integration/UITests.kt
+++ src/test/com/miyagi/shashin/integration/UITests.kt
@@ -1,6 +1,5 @@
 package com.miyagi.shashin.integration
 
-import ai.djl.Model
 import com.miyagi.shashin.e2e.BaseSeleniumTests
 import com.miyagi.shashin.model.User
 import com.miyagi.shashin.repository.UserRepository
@@ -15,19 +14,16 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.server.LocalServerPort
 import org.springframework.context.MessageSource
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers
 import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
-import org.springframework.ui.set
-import org.springframework.util.StringUtils
 import org.springframework.web.context.WebApplicationContext
-import org.springframework.web.servlet.support.RequestContextUtils
+import java.io.FileInputStream
 import java.io.IOException
 import java.util.Locale
-import kotlin.text.get
+import java.util.Properties
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
@@ -45,6 +41,9 @@ class UITests: BaseSeleniumTests() {
 
     @Autowired
     private val userRepository: UserRepository? = null
+
+    @Autowired
+    var messageSource: MessageSource? = null
 
     private var bcrypt = BCryptPasswordEncoder()
 
@@ -80,6 +79,7 @@ class UITests: BaseSeleniumTests() {
         val password = this.driver!!.findElement(By.id("password"))
         val rememberMe = this.driver!!.findElement(By.id("remember-me"))
         val login = this.driver!!.findElement(By.id("submit-loginreg"))
+
         rememberMe.click()
         username.sendKeys("testadmin")
         password.sendKeys("testadmin")
@@ -258,5 +258,277 @@ class UITests: BaseSeleniumTests() {
         Assertions.assertTrue(activeClass == "page-item active")
         var pagyPage = js.executeScript("return document.getElementById('pagyPage').value")
         Assertions.assertTrue(pagyPage?.toString()?.toInt() == currentPage)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun jsAndKotlinTranslationsMatch() {
+        val enProperties = Properties()
+        try {
+            FileInputStream("src/main/resources/messages.properties").use { fis ->
+                enProperties.load(fis)
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        var locale = Locale("en")
+
+        this.driver!!.get("http://localhost:$port/test")
+        val js: JavascriptExecutor = this.driver as JavascriptExecutor
+
+        @Suppress("UNCHECKED_CAST")
+        val keyValTranslation: Map<String, String> = js.executeScript("return shashini18n;") as Map<String, String>
+        Assertions.assertEquals(keyValTranslation.size, enProperties.stringPropertyNames().size)
+        // Keys not present in either map
+        Assertions.assertEquals((keyValTranslation.keys - enProperties.stringPropertyNames()).size, 0)
+        Assertions.assertEquals((enProperties.stringPropertyNames() - keyValTranslation.keys).size, 0)
+
+        // English
+        var index = 0
+        for (key in enProperties.stringPropertyNames()) {
+            index++
+//            var value = properties.getProperty(key)
+//            println("Key: $key, Value: $value")
+
+            var val1a = "Bob"
+            var val1b = 5
+            var val2 = "Alice"
+            var val3 = "Max"
+            var val4 = "Linda"
+            var val5 = "Joe"
+            var val6 = "Matthew"
+            var val7 = "Jacob"
+            var val8 = "Mike"
+            var val9 = "Irene"
+
+            var useInt = false
+
+            if (key == "main.pages.albums.photo" ||
+                key == "main.pages.people.items" ||
+                key == "main.pages.albums.video" ||
+                key == "main.pages.map.modal.result"
+            ) {
+                useInt = true
+            }
+
+            var jsTranslation = if (key == "main.pages.scan.training.similarity" || key == "main.pages.scan.training.similarity.label") {
+                js.executeScript("const translatedValue = shashin.getTranslatedValue('" + key + "', '" + val1a + "', '" + val2 + "', '" + val3 + "', '" + val4 + "', '" + val5 + "', '" + val6 + "', '" + val7 + "', '" + val8 + "', '" + val9 + "'); return translatedValue;")
+            } else if (key == "main.toast.topnav.selected" || key == "main.pages.match.info") {
+                js.executeScript("const translatedValue = shashin.getTranslatedValue('" + key + "', " + val1b + "); return translatedValue;")
+            } else if (key == "main.notification.comments.photo.commented") {
+                js.executeScript("const translatedValue = shashin.getTranslatedValue('" + key + "', '" + val1a + "', '" + val2 + "', '" + val3 + "', '" + val4 + "'); return translatedValue;")
+            } else if (key == "main.notification.comments.commented") {
+                js.executeScript("const translatedValue = shashin.getTranslatedValue('"+key+"', '"+val1b+"', '"+val2+"', '"+val3+"'); return translatedValue;")
+            } else if (key == "main.pages.matching.processing" || key == "main.notification.album.shared.access") {
+                js.executeScript("const translatedValue = shashin.getTranslatedValue('"+key+"', '"+val1a+"', '"+val2+"', "+val1b+"); return translatedValue;")
+            } else if (key == "main.pages.albums.photo" || key == "main.pages.people.items" || key == "main.pages.albums.video" || key == "main.pages.map.modal.result") {
+                js.executeScript("const translatedValue = shashin.getTranslatedValue('"+key+"', '"+val1b+"', '"+val2+"'); return translatedValue;")
+            } else {
+                js.executeScript("const translatedValue = shashin.getTranslatedValue('"+key+"', '"+val1a+"', '"+val2+"'); return translatedValue;")
+            }
+
+//            println("JS translation:"+jsTranslation)
+
+            var kotlinTranslation = if (key == "main.pages.scan.training.similarity" || key == "main.pages.scan.training.similarity.label") {
+                messageSource?.getMessage(key, arrayOf(val1a, val2, val3, val4, val5, val6, val7, val8, val9), locale)
+            } else if (key == "main.toast.topnav.selected" || key == "main.pages.match.info") {
+                messageSource?.getMessage(key, arrayOf(val1b), locale)
+            } else if (key == "main.notification.comments.photo.commented") {
+                messageSource?.getMessage(key, arrayOf(val1a, val2, val3, val4), locale)
+            } else if (key == "main.notification.comments.commented") {
+                messageSource?.getMessage(key, arrayOf(val1b, val2, val3), locale)
+            } else if (key == "main.pages.matching.processing" || key == "main.notification.album.shared.access") {
+                messageSource?.getMessage(key, arrayOf(val1a, val2, val1b), locale)
+            } else if (key == "main.pages.albums.photo" || key == "main.pages.people.items" || key == "main.pages.albums.video" || key == "main.pages.map.modal.result") {
+                val mixedArray: Array<Any> = arrayOf(val1b, val2)
+                messageSource?.getMessage(key, mixedArray, locale)
+            } else {
+                messageSource?.getMessage(key, arrayOf(val1a, val2), locale)
+            }
+//            println("kotlinTranslation:"+kotlinTranslation)
+
+//            if (jsTranslation != kotlinTranslation) {
+//                println("translations do not match")
+//            }
+
+            Assertions.assertEquals(jsTranslation, kotlinTranslation)
+
+//            println("--------------")
+        }
+
+        Assertions.assertTrue(index > 0 && index == enProperties.stringPropertyNames().size)
+
+        // German
+        val deProperties = Properties()
+        try {
+            FileInputStream("src/main/resources/messages_de.properties").use { fis ->
+                deProperties.load(fis)
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        Assertions.assertEquals(keyValTranslation.size, deProperties.stringPropertyNames().size)
+        Assertions.assertEquals((keyValTranslation.keys - deProperties.stringPropertyNames()).size, 0)
+        Assertions.assertEquals((deProperties.stringPropertyNames() - keyValTranslation.keys).size, 0)
+
+        for (key in deProperties.stringPropertyNames()) {
+            val regex = Regex("""\{(\d+)}""")
+            var deValue = deProperties.getProperty(key)
+            var enValue = enProperties.getProperty(key)
+            var matches = regex.findAll(deValue)
+            val deValueCount = matches.count()
+            matches = regex.findAll(enValue)
+            val enValueCount = matches.count()
+
+            Assertions.assertEquals(enValueCount, deValueCount)
+            Assertions.assertTrue(enProperties.stringPropertyNames().contains(key))
+        }
+
+        // Spanish
+        val esProperties = Properties()
+        try {
+            FileInputStream("src/main/resources/messages_es.properties").use { fis ->
+                esProperties.load(fis)
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        Assertions.assertEquals(keyValTranslation.size, esProperties.stringPropertyNames().size)
+        Assertions.assertEquals((keyValTranslation.keys - esProperties.stringPropertyNames()).size, 0)
+        Assertions.assertEquals((esProperties.stringPropertyNames() - keyValTranslation.keys).size, 0)
+
+        for (key in esProperties.stringPropertyNames()) {
+            val regex = Regex("""\{(\d+)}""")
+            var esValue = esProperties.getProperty(key)
+            var enValue = enProperties.getProperty(key)
+            var matches = regex.findAll(esValue)
+            val esCount = matches.count()
+            matches = regex.findAll(enValue)
+            val enCount = matches.count()
+
+            Assertions.assertEquals(enCount, esCount)
+            Assertions.assertTrue(enProperties.stringPropertyNames().contains(key))
+        }
+
+        // French
+        val frProperties = Properties()
+        try {
+            FileInputStream("src/main/resources/messages_fr.properties").use { fis ->
+                frProperties.load(fis)
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        Assertions.assertEquals(keyValTranslation.size, frProperties.stringPropertyNames().size)
+        Assertions.assertEquals((keyValTranslation.keys - frProperties.stringPropertyNames()).size, 0)
+        Assertions.assertEquals((frProperties.stringPropertyNames() - keyValTranslation.keys).size, 0)
+
+        for (key in frProperties.stringPropertyNames()) {
+            val regex = Regex("""\{(\d+)}""")
+            var frValue = frProperties.getProperty(key)
+            var enValue = enProperties.getProperty(key)
+            var matches = regex.findAll(frValue)
+            val frValueCount = matches.count()
+            matches = regex.findAll(enValue)
+            val enValueCount = matches.count()
+
+            Assertions.assertEquals(enValueCount, frValueCount)
+            Assertions.assertTrue(enProperties.stringPropertyNames().contains(key))
+        }
+
+        // Japanese
+        val jaProperties = Properties()
+        try {
+            FileInputStream("src/main/resources/messages_ja.properties").use { fis ->
+                jaProperties.load(fis)
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        Assertions.assertEquals(keyValTranslation.size, jaProperties.stringPropertyNames().size)
+        Assertions.assertEquals((keyValTranslation.keys - jaProperties.stringPropertyNames()).size, 0)
+        Assertions.assertEquals((jaProperties.stringPropertyNames() - keyValTranslation.keys).size, 0)
+
+        for (key in jaProperties.stringPropertyNames()) {
+            val regex = Regex("""\{(\d+)}""")
+            var jpValue = jaProperties.getProperty(key)
+            var enValue = enProperties.getProperty(key)
+            var matches = regex.findAll(jpValue)
+            val jpValueCount = matches.count()
+            matches = regex.findAll(enValue)
+            val enValueCount = matches.count()
+
+            // Japanese singular only
+            if (key == "main.pages.albums.photo" ||
+                key == "main.pages.people.items" ||
+                key == "main.pages.albums.video" ||
+                key == "main.pages.map.modal.result"
+            ) {
+                Assertions.assertEquals(1, jpValueCount)
+            } else {
+                Assertions.assertEquals(enValueCount, jpValueCount)
+            }
+
+            Assertions.assertTrue(enProperties.stringPropertyNames().contains(key))
+        }
+
+        // Portuguese
+        val ptProperties = Properties()
+        try {
+            FileInputStream("src/main/resources/messages_pt.properties").use { fis ->
+                ptProperties.load(fis)
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        Assertions.assertEquals(keyValTranslation.size, ptProperties.stringPropertyNames().size)
+        Assertions.assertEquals((keyValTranslation.keys - ptProperties.stringPropertyNames()).size, 0)
+        Assertions.assertEquals((ptProperties.stringPropertyNames() - keyValTranslation.keys).size, 0)
+
+        for (key in ptProperties.stringPropertyNames()) {
+            val regex = Regex("""\{(\d+)}""")
+            var ptValue = ptProperties.getProperty(key)
+            var enValue = enProperties.getProperty(key)
+            var matches = regex.findAll(ptValue)
+            val ptValueCount = matches.count()
+            matches = regex.findAll(enValue)
+            val enValueCount = matches.count()
+
+            Assertions.assertEquals(enValueCount, ptValueCount)
+            Assertions.assertTrue(enProperties.stringPropertyNames().contains(key))
+        }
+
+        // Russian
+        val ruProperties = Properties()
+        try {
+            FileInputStream("src/main/resources/messages_ru.properties").use { fis ->
+                ruProperties.load(fis)
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        Assertions.assertEquals(keyValTranslation.size, ruProperties.stringPropertyNames().size)
+        Assertions.assertEquals((keyValTranslation.keys - ruProperties.stringPropertyNames()).size, 0)
+        Assertions.assertEquals((ruProperties.stringPropertyNames() - keyValTranslation.keys).size, 0)
+
+        for (key in ruProperties.stringPropertyNames()) {
+            val regex = Regex("""\{(\d+)}""")
+            var ruValue = ruProperties.getProperty(key)
+            var enValue = enProperties.getProperty(key)
+            var matches = regex.findAll(ruValue)
+            val ruValueCount = matches.count()
+            matches = regex.findAll(enValue)
+            val enValueCount = matches.count()
+
+            Assertions.assertEquals(enValueCount, ruValueCount)
+            Assertions.assertTrue(enProperties.stringPropertyNames().contains(key))
+        }
     }
 }
