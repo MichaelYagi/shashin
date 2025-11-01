454927b0fe7f709f24d2d264e8c24c88fc6514fc
Michael Yagi <michaeltyagi@gmail.com>
2025-10-31 22:04:04 -0700

    [skip ci] Made sandbox public

--- src/main/kotlin/com/miyagi/shashin/configuration/WebSecurityConfig.kt
+++ src/main/kotlin/com/miyagi/shashin/configuration/WebSecurityConfig.kt
@@ -93,6 +93,7 @@ class MultiSecurityConfig {
             "/articles/**",
             "/health",
             "/testgrounds",
+            "/sandbox",
             "/bcrypt/**",
             "/features",
             "/css/**",
