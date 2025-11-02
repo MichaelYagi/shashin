1c327d1862aebddfe9032437f0003253c3178d60
Michael Yagi <michaeltyagi@gmail.com>
2025-11-02 00:04:03 -0700

    [skip ci] Fixed paged share video thumbnails

--- src/main/resources/static/js/lg-plugin-template.js
+++ src/main/resources/static/js/lg-plugin-template.js
@@ -1,20 +1,26 @@
 /**
  * MY - Added Oct 28 2023
- * - Working Plugin template
- * lightGallery(document.getElementById('lightgallery'), {
- *   plugins: [lgPluginName],
- *   licenseKey: 'your_license_key',
- *   speed: 500,
- *   pluginSettingAttribute: true,
- *   pluginFunctionName: someFunction
- *   // ... other settings
- * });
+ * - Working Plugin template. See below example.
+ *
+ * function pluginFunction(metadataId, lgId, lgIndex) {}
+ *
+ * const configs = {
+ *     plugins: [lgPluginName],
+ *     pluginSettingAttribute: true,
+ *     pluginFunctionName: pluginFunction, // Optional, see below
+ *     // ... other settings
+ * };
+ *
+ * const lg = lightGallery(lgEl, configs);
+ *
+ * lg.openGallery();
  */
 
 ! function(_window, pluginFunction) {
     // TODO: Define the plugin name here
     let pluginName = "lgPluginName";
 
+    // Boilerplate
     if ("object" == typeof exports && "undefined" != typeof module) {
         module.exports = l();
     } else if ("function" == typeof define && define.amd) {
@@ -23,6 +29,7 @@
         (_window = "undefined" != typeof globalThis ? globalThis : _window || self)[pluginName] = pluginFunction();
     }
 } (this, (function() {
+    // Boilerplate
     "use strict";
     let __assign = function () {
         __assign = Object.assign || function __assign(t) {
@@ -35,20 +42,20 @@
         return __assign.apply(this, arguments);
     };
 
-    // TODO: Define attributes to be used in setting
+    // TODO: Define attributes and set in configs
     const pluginSettings = {
         pluginSettingAttribute: true
     };
 
     return (function() {
-        // TODO: Define class
+        // TODO: Define class. Change class name in line with functionality.
         function Plugin(instance, $LG) {
             // get lightGallery core plugin instance
             this.core = instance;
 
             this.$LG = $LG;
 
-            // TODO: Extend module default settings with lightGallery core settings
+            // Extends module default settings with lightGallery core settings
             this.settings = __assign(__assign({}, pluginSettings), this.core.settings);
 
             return this;
@@ -56,40 +63,64 @@
 
         // TODO: Initialize the plugin
         Plugin.prototype.init = function () {
-            // TODO: Initialize the plugin icon
+            // TODO: Define the plugin icon
             const menuIcon = "bi-0-circle";
 
-            // TODO: Get from pluginSettings variable
+            // TODO: Get from attribute in pluginSettings
             if (this.settings.pluginSettingAttribute) {
-                // TODO: Initialize the plugin button
+                // TODO: Initialize the plugin toolbar button
                 const pluginMenuItem = '<button type="button" aria-label="Plugin Name" title="Plugin Name" id="captureThumbnail" class="'+menuIcon+' lg-icon" style="font-size: 1rem;"></button>';
 
                 this.core.$toolbar.append(pluginMenuItem);
 
+                // Additional optional init call
                 this.plugin();
             }
 
             this.core.LGel.off('lgBeforeSlide').on('lgBeforeSlide', (e) => {
                 console.log("You are on slide " + e.detail.index);
+
+                shashin.closeToastMessages();
             });
 
-            // TODO: Define what happens when clicking the menu button
+            // TODO: Define what happens when clicking the button in the menu
             this.core.outer
                 .find('.'+menuIcon)
                 .first()
                 .off('click.lg')
                 .on('click.lg', () => {
-                    alert("This is a template for setting up a plugin!");
+                    if (typeof shashin !== 'undefined') {
+                        shashin.showToastMessage(null, "This is a template for setting up a plugin!", {
+                            tag: "lgPluginName",
+                            autohide: false,
+                            closeButton: true
+                        });
+                    } else {
+                        console.log("This is a template for setting up a plugin!");
+                    }
 
+                    /* This is an optional call from the settings depending on integration */
+                    // Or call an external function, eg. shashin.showToastMessage
                     const funObject = Util.getLgFunction(this, "pluginFunctionName");
 
                     // Execute function
                     if (typeof funObject.fn === 'function' && funObject.args !== null && funObject.args.length > 0) {
-                        $("#metadataId").val(funObject.args);
+                        // if this.settings.pluginFunctionName is found in settings, metadataId will be in funObject.args
                         funObject.fn(funObject.args, this.core.lgId, this.core.index);
-                    } else if (shashin) {
-                        shashin.showToastMessage(shashin.getTranslatedValue("main.toast.lgplugin.title"), shashin.getTranslatedValue("main.toast.lgplugin.body"), {icon:"bi-exclamation-triangle", iconColor:"#FF0000", borderColor:"danger"});
+                    } else if (typeof shashin !== 'undefined') {
+                        shashin.showToastMessage(shashin.getTranslatedValue("main.toast.lgplugin.title"),
+                            "Config property pluginFunctionName not defined in configs",
+                            {
+                                icon:"bi-exclamation-triangle",
+                                iconColor:"#FF0000",
+                                autohide: false,
+                                borderColor:"danger"
+                            }
+                        );
+                    } else {
+                        console.warn("Config property pluginFunctionName not defined in config");
                     }
+                    /* End optional call */
                 });
         };
 
