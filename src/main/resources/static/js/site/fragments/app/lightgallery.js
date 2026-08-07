a84bdea65ddd68e3ffadf139bfbea1a92feb70a3
Michael Yagi <michaeltyagi@gmail.com>
2026-08-07 07:35:47 -0700

    Fixed tests

--- src/main/resources/static/js/site/fragments/app/lightgallery.js
+++ src/main/resources/static/js/site/fragments/app/lightgallery.js
@@ -355,7 +355,7 @@
             configs = {
                 plugins: [Shoji.Zoom, Shoji.Fullscreen, ...rotateFlipPlugin, ...downloadPlugin, ...castPlugin, ...editorPlugin],
                 counter: false,
-                preload: 1,
+                preload: 2,
                 autoHideDelay: 5000,
                 mode: 'fade',
                 download: (typeof lgDownload !== 'undefined'),
